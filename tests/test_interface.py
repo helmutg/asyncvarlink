f1db1784df54b6d8d3f6bae003a5e1578340e1f6
Helmut Grohne <helmut@subdivi.de>
2024-09-14 14:15:03 +0200

    add tests for VarlinkServiceInterface

---
+++ tests/test_interface.py
@@ -0,0 +1,36 @@
+import unittest
+
+from asyncvarlink import VarlinkServiceInterface
+
+
+class TestServiceInterface(unittest.TestCase):
+    def setUp(self) -> None:
+        self.vsi = VarlinkServiceInterface(
+            "examplevendor", "exampleproduct", "1.0", "https://example.com"
+        )
+        self.vsi.register(self.vsi)
+
+    def test_getinfo(self) -> None:
+        self.assertEqual(self.vsi.name, "org.varlink.service")
+        self.assertEqual(
+            {
+                "vendor": "examplevendor",
+                "product": "exampleproduct",
+                "version": "1.0",
+                "url": "https://example.com",
+                "interfaces": [self.vsi.name],
+            },
+            self.vsi.GetInfo(),
+        )
+
+    def test_description(self) -> None:
+        self.assertEqual(
+            {
+                "description": """interface org.varlink.service
+
+method GetInfo() -> (interfaces: []string, product: string, url: string, vendor: string, version: string)
+method GetInterfaceDescription(interface: string) -> (description: string)
+""",
+            },
+            self.vsi.GetInterfaceDescription(interface=self.vsi.name),
+        )

helmutg/asyncvarlink

commit 5e6fa26b411967d638c55609615b9ebf18f3da12

validate the name attribute on VarlinkInterface subclasses

asyncvarlink/interface.py
@@ -352,6 +352,21 @@ class VarlinkInterface:
     name: str
     """The name of the varlink interface in dotted reverse domain notation."""
 
+    def __init_subclass__(
+        cls: type["VarlinkInterface"], *, name: str | None = None
+    ) -> None:
+        if cls.name is None:
+            if name is None:
+                raise RuntimeError(
+                    "VarlinkInterface subclasses must define an interface name"
+                )
+            cls.name = name
+        elif name is not None:
+            raise RuntimeError(
+                "cannot specify VarlinkInterface name both via inheritance "
+                "and attribute"
+            )
+
     def render_interface_description(self) -> str:
         """Render a varlink interface description from this interface.
         Refer to https://varlink.org/Interface-Definition.

commit 11e6128ed1b811151ed28adba85a82fa810d514c

VarlinkInterface: fix passing name as inheritance parameter
Fixes: 5e6fa26b4119 ("validate the name attribute on VarlinkInterface subclasses")

asyncvarlink/interface.py
@@ -359,17 +359,20 @@ class VarlinkInterface:
     def __init_subclass__(
         cls: type["VarlinkInterface"], *, name: str | None = None
     ) -> None:
-        if cls.name is None:
+        try:
+            cls.name
+        except AttributeError:
             if name is None:
                 raise RuntimeError(
                     "VarlinkInterface subclasses must define an interface name"
-                )
+                ) from None
             cls.name = name
-        elif name is not None:
-            raise RuntimeError(
-                "cannot specify VarlinkInterface name both via inheritance "
-                "and attribute"
-            )
+        else:
+            if name is not None:
+                raise RuntimeError(
+                    "cannot specify VarlinkInterface name both via "
+                    "inheritance and attribute"
+                )
         try:
             validate_interface(cls.name)
         except ValueError as err:

asyncvarlink/serviceinterface.py
@@ -63,10 +63,8 @@ class VarlinkInterfaceRegistry:
         return self.interfaces[interface]
 
 
-class VarlinkServiceInterface(VarlinkInterface):
+class VarlinkServiceInterface(VarlinkInterface, name="org.varlink.service"):
     """Implementation of the basic varlink introspection interface."""
-
-    name = "org.varlink.service"
 
     class _GetInfoResult(typing.TypedDict):
         vendor: str
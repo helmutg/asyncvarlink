039b68a647091c7a0a8cad4a22eeaa3e15d95d6f
Helmut Grohne <helmut@subdivi.de>
2024-10-19 12:25:19 +0200

    actually add VarlinkInterfaceServerProtocol class
    This glues the VarlinkServerProtocol and the interface module together
    and allows for conveniently running a service.

--- asyncvarlink/serverprotocol.py
+++ asyncvarlink/serverprotocol.py
@@ -4,10 +4,14 @@
 """asyncio varlink server protocol implementation"""
 
 import asyncio
+import typing
 
+from .conversion import ConversionError, FileDescriptorVarlinkType
 from .error import VarlinkErrorReply, GenericVarlinkErrorReply
+from .interface import AnnotatedResult, VarlinkMethodSignature
 from .message import VarlinkMethodCall, VarlinkMethodReply
 from .protocol import VarlinkProtocol
+from .serviceinterface import InvalidParameter, VarlinkInterfaceRegistry
 from .types import FileDescriptorArray, JSONObject
 
 
@@ -53,3 +57,131 @@ class VarlinkServerProtocol(VarlinkProtocol):
         VarlinkErrorReply to be sent by the caller.
         """
         raise NotImplementedError
+
+
+class VarlinkInterfaceServerProtocol(VarlinkServerProtocol):
+    """Serve the interfaces registered with a registry via varlink."""
+
+    def __init__(self, registry: VarlinkInterfaceRegistry) -> None:
+        """Method lookup is deferred to the given registry."""
+        super().__init__()
+        self._registry = registry
+
+    def call_received(
+        self, call: VarlinkMethodCall, fds: FileDescriptorArray | None
+    ) -> asyncio.Future[None] | None:
+        method, signature = self._registry.lookup_method(call)
+        try:
+            pyparams = signature.parameter_type.fromjson(
+                call.parameters, {FileDescriptorVarlinkType: fds}
+            )
+        except ConversionError as err:
+            raise InvalidParameter(parameter=err.location[0]) from err
+        if not signature.asynchronous:
+            if signature.more:
+                return asyncio.ensure_future(
+                    self._call_sync_method_more(method, signature, pyparams)
+                )
+            self._call_sync_method_single(
+                method, signature, pyparams, call.oneway
+            )
+            return None
+        if signature.more:
+            return asyncio.ensure_future(
+                self._call_async_method_more(method, signature, pyparams)
+            )
+        return asyncio.ensure_future(
+            self._call_async_method_single(
+                method, signature, pyparams, call.oneway
+            ),
+        )
+
+    def _call_sync_method_single(
+        self,
+        method: typing.Callable[..., typing.Any],
+        signature: VarlinkMethodSignature,
+        pyparams: dict[str, typing.Any],
+        oneway: bool,
+    ) -> asyncio.Future[None] | None:
+        result = method(**pyparams)
+        assert isinstance(result, AnnotatedResult)
+        assert not result.continues
+        if oneway:
+            return None
+        fds: list[int] = []  # modified by tojson
+        jsonparams = signature.return_type.tojson(
+            result.parameters, {FileDescriptorVarlinkType: fds}
+        )
+        return self.send_reply(
+            VarlinkMethodReply(jsonparams, extensions=result.extensions), fds
+        )
+
+    async def _call_sync_method_more(
+        self,
+        method: typing.Callable[..., typing.Any],
+        signature: VarlinkMethodSignature,
+        pyparams: dict[str, typing.Any],
+    ) -> None:
+        continues = True
+        for result in method(**pyparams):
+            assert continues
+            assert isinstance(result, AnnotatedResult)
+            fds: list[int] = []  # modified by tojson
+            jsonparams = signature.return_type.tojson(
+                result.parameters, {FileDescriptorVarlinkType: fds}
+            )
+            await self.send_reply(
+                VarlinkMethodReply(
+                    jsonparams,
+                    continues=result.continues,
+                    extensions=result.extensions,
+                ),
+                fds,
+            )
+            continues = result.continues
+        assert not continues
+
+    async def _call_async_method_single(
+        self,
+        method: typing.Callable[..., typing.Any],
+        signature: VarlinkMethodSignature,
+        pyparams: dict[str, typing.Any],
+        oneway: bool,
+    ) -> None:
+        result = await method(**pyparams)
+        assert isinstance(result, AnnotatedResult)
+        assert not result.continues
+        if oneway:
+            return
+        fds: list[int] = []  # modified by tojson
+        jsonparams = signature.return_type.tojson(
+            result.parameters, {FileDescriptorVarlinkType: fds}
+        )
+        await self.send_reply(
+            VarlinkMethodReply(jsonparams, extensions=result.extensions), fds
+        )
+
+    async def _call_async_method_more(
+        self,
+        method: typing.Callable[..., typing.Any],
+        signature: VarlinkMethodSignature,
+        pyparams: dict[str, typing.Any],
+    ) -> None:
+        continues = True
+        async for result in method(**pyparams):
+            assert continues
+            assert isinstance(result, AnnotatedResult)
+            fds: list[int] = []  # modified by tojson
+            jsonparams = signature.return_type.tojson(
+                result.parameters, {FileDescriptorVarlinkType: fds}
+            )
+            await self.send_reply(
+                VarlinkMethodReply(
+                    jsonparams,
+                    continues=result.continues,
+                    extensions=result.extensions,
+                ),
+                fds,
+            )
+            continues = result.continues
+        assert not continues

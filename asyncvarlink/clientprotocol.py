helmutg/asyncvarlink

commit dc65d0e43407e023640982bd0b508f352fe42016

VarlinkClientProtocol: add convenience method for making proxies
Reported-by: Jochen Sprickerhof <git@jochen.sprickerhof.de>

asyncvarlink/clientprotocol.py
@@ -158,6 +158,12 @@ class VarlinkClientProtocol(VarlinkProtocol):
             pending = self._pending.popleft()
         pending.request_received(obj, fds)
 
+    def make_proxy(self, interface: type[VarlinkInterface]) -> "VarlinkInterfaceProxy":
+        """Convenience function that constructs an interface proxy object for
+        the combination of protocol instance and interface class at hand.
+        """
+        return VarlinkInterfaceProxy(self, interface)
+
 
 class VarlinkInterfaceProxy:
     """Interface proxy class for a varlink client. Its only purpose is to

tests/test_clientprotocol.py
@@ -8,7 +8,6 @@ import unittest
 from asyncvarlink import (
     VarlinkClientProtocol,
     VarlinkInterface,
-    VarlinkInterfaceProxy,
     VarlinkTransport,
     varlinkmethod,
 )
@@ -29,7 +28,7 @@ class ClientTests(unittest.IsolatedAsyncioTestCase):
         try:
             proto = VarlinkClientProtocol()
             transport = VarlinkTransport(loop, sock2, sock2, proto)
-            proxy = VarlinkInterfaceProxy(proto, DemoInterface)
+            proxy = proto.make_proxy(DemoInterface)
             fut = asyncio.ensure_future(proxy.Method(argument="spam"))
             data = await loop.sock_recv(sock1, 1024)
             self.assertEqual(

tests/test_end2end.py
@@ -9,7 +9,6 @@ import unittest
 from asyncvarlink import (
     VarlinkClientProtocol,
     VarlinkInterface,
-    VarlinkInterfaceProxy,
     VarlinkInterfaceRegistry,
     VarlinkInterfaceServerProtocol,
     connect_unix_varlink,
@@ -48,7 +47,7 @@ class End2EndTests(unittest.IsolatedAsyncioTestCase):
             )
             stack.callback(transport.close)
             assert isinstance(protocol, VarlinkClientProtocol)
-            proxy = VarlinkInterfaceProxy(protocol, DummyInterface)
+            proxy = protocol.make_proxy(DummyInterface)
             self.assertEqual(
                 await proxy.Method(argument="argument"),
                 {"result": "returnvalue"},

commit c6f7aba0cf35879d8cebaa92206d07f3d42479d4

fix formatting for black
Fixes: dc65d0e43407 ("VarlinkClientProtocol: add convenience method for making proxies")

asyncvarlink/clientprotocol.py
@@ -158,7 +158,9 @@ class VarlinkClientProtocol(VarlinkProtocol):
             pending = self._pending.popleft()
         pending.request_received(obj, fds)
 
-    def make_proxy(self, interface: type[VarlinkInterface]) -> "VarlinkInterfaceProxy":
+    def make_proxy(
+        self, interface: type[VarlinkInterface]
+    ) -> "VarlinkInterfaceProxy":
         """Convenience function that constructs an interface proxy object for
         the combination of protocol instance and interface class at hand.
         """
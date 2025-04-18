8205b481ca5933230e50a4a952bbbe27ed2c27bd
Helmut Grohne <helmut@subdivi.de>
2025-04-18 19:10:14 +0200

    test_serverprotocol.py: add case for raising an error

--- tests/test_serverprotocol.py
+++ tests/test_serverprotocol.py
@@ -6,6 +6,7 @@ import socket
 import unittest
 
 from asyncvarlink import (
+    TypedVarlinkErrorReply,
     VarlinkInterface,
     VarlinkInterfaceRegistry,
     VarlinkInterfaceServerProtocol,
@@ -14,10 +15,19 @@ from asyncvarlink import (
 )
 
 
+class DemoError(TypedVarlinkErrorReply, interface="com.example.demo"):
+    class Parameters:
+        pass
+
+
 class DemoInterface(VarlinkInterface, name="com.example.demo"):
     @varlinkmethod(return_parameter="result")
     def Answer(self) -> int:
         return 42
+
+    @varlinkmethod
+    def Error(self) -> None:
+        raise DemoError()
 
 
 class ServerTests(unittest.IsolatedAsyncioTestCase):
@@ -26,11 +36,12 @@ class ServerTests(unittest.IsolatedAsyncioTestCase):
         self.registry = VarlinkInterfaceRegistry()
         self.registry.register_interface(DemoInterface())
 
-    async def test_smoke(self) -> None:
+    async def invoke(self, request: bytes, expected_response: bytes) -> None:
         loop = asyncio.get_running_loop()
         sock1, sock2 = socket.socketpair(
             type=socket.SOCK_STREAM | socket.SOCK_NONBLOCK
         )
+        transport: VarlinkTransport | None = None
         try:
             transport = VarlinkTransport(
                 loop,
@@ -38,11 +49,9 @@ class ServerTests(unittest.IsolatedAsyncioTestCase):
                 sock2,
                 VarlinkInterfaceServerProtocol(self.registry),
             )
-            await loop.sock_sendall(
-                sock1, b'{"method":"com.example.demo.Answer"}\0'
-            )
+            await loop.sock_sendall(sock1, request + b"\0")
             data = await loop.sock_recv(sock1, 1024)
-            self.assertEqual(data, b'{"parameters":{"result":42}}\0')
+            self.assertEqual(data, expected_response + b"\0")
         finally:
             if transport:
                 transport.close()
@@ -51,3 +60,15 @@ class ServerTests(unittest.IsolatedAsyncioTestCase):
             else:
                 sock2.close()
             sock1.close()
+
+    async def test_smoke(self) -> None:
+        await self.invoke(
+            b'{"method":"com.example.demo.Answer"}',
+            b'{"parameters":{"result":42}}',
+        )
+
+    async def test_error(self) -> None:
+        await self.invoke(
+            b'{"method":"com.example.demo.Error"}',
+            b'{"error":"com.example.demo.DemoError"}',
+        )

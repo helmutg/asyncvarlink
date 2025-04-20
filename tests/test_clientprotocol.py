4a6a68ecfde20d1c42a4a55a9e58cb218ecfaf2c
Helmut Grohne <helmut@subdivi.de>
2025-04-20 20:38:52 +0200

    test_clientprotocol.py: refactor to ease writing more tests

--- tests/test_clientprotocol.py
+++ tests/test_clientprotocol.py
@@ -23,52 +23,56 @@ class DemoInterface(VarlinkInterface, name="com.example.demo"):
 
 
 class ClientTests(unittest.IsolatedAsyncioTestCase):
-    async def test_smoke(self) -> None:
-        loop = asyncio.get_running_loop()
-        sock1, sock2 = socket.socketpair(
+    async def asyncSetUp(self) -> None:
+        await super().asyncSetUp()
+        self.loop = asyncio.get_running_loop()
+        self.sock1, self.sock2 = socket.socketpair(
             type=socket.SOCK_STREAM | socket.SOCK_NONBLOCK
         )
-        transport = None
-        try:
-            proto = VarlinkClientProtocol()
-            transport = VarlinkTransport(loop, sock2, sock2, proto)
-            proxy = proto.make_proxy(DemoInterface)
-            fut = asyncio.ensure_future(proxy.Method(argument="spam"))
-            data = await loop.sock_recv(sock1, 1024)
-            self.assertEqual(
-                data,
-                b'{"method":"com.example.demo.Method","parameters":{"argument":"spam"}}\0',
-            )
-            self.assertFalse(fut.done())
-            await loop.sock_sendall(
-                sock1, b'{"parameters":{"result":"egg"}}\0'
-            )
-            self.assertEqual(await fut, {"result": "egg"})
-            gen = proxy.MoreMethod()
-            fut = asyncio.ensure_future(anext(gen))
-            data = await loop.sock_recv(sock1, 1024)
-            self.assertEqual(
-                data, b'{"method":"com.example.demo.MoreMethod","more":true}\0'
-            )
-            self.assertFalse(fut.done())
-            await loop.sock_sendall(
-                sock1, b'{"continues":true,"parameters":{"result":"spam"}}\0'
-            )
-            self.assertEqual(await fut, {"result": "spam"})
-            fut = asyncio.ensure_future(anext(gen))
-            await asyncio.sleep(0)
-            self.assertFalse(fut.done())
-            await loop.sock_sendall(
-                sock1, b'{"parameters":{"result":"egg"}}\0'
-            )
-            self.assertEqual(await fut, {"result": "egg"})
-            with self.assertRaises(StopAsyncIteration):
-                await anext(gen)
-        finally:
-            if transport:
-                transport.close()
-                await asyncio.sleep(0)
-                self.assertLess(sock2.fileno(), 0)
-            else:
-                sock2.close()
-            sock1.close()
+        self.proto = VarlinkClientProtocol()
+        self.transport = VarlinkTransport(
+            self.loop, self.sock2, self.sock2, self.proto
+        )
+        self.proxy = self.proto.make_proxy(DemoInterface)
+
+    async def asyncTearDown(self) -> None:
+        self.transport.close()
+        await asyncio.sleep(0)
+        self.assertLess(self.sock2.fileno(), 0)
+        self.sock1.close()
+        await super().asyncTearDown()
+
+    async def expect_data(self, expected: bytes) -> None:
+        data = await self.loop.sock_recv(self.sock1, len(expected) + 1)
+        self.assertEqual(data, expected)
+
+    async def send_data(self, data: bytes) -> None:
+        await self.loop.sock_sendall(self.sock1, data)
+
+    async def test_simple(self) -> None:
+        fut = asyncio.ensure_future(self.proxy.Method(argument="spam"))
+        await self.expect_data(
+            b'{"method":"com.example.demo.Method","parameters":{"argument":"spam"}}\0'
+        )
+        self.assertFalse(fut.done())
+        await self.send_data(b'{"parameters":{"result":"egg"}}\0')
+        self.assertEqual(await fut, {"result": "egg"})
+
+    async def test_more(self) -> None:
+        gen = self.proxy.MoreMethod()
+        fut = asyncio.ensure_future(anext(gen))
+        await self.expect_data(
+            b'{"method":"com.example.demo.MoreMethod","more":true}\0'
+        )
+        self.assertFalse(fut.done())
+        await self.send_data(
+            b'{"continues":true,"parameters":{"result":"spam"}}\0'
+        )
+        self.assertEqual(await fut, {"result": "spam"})
+        fut = asyncio.ensure_future(anext(gen))
+        await asyncio.sleep(0)
+        self.assertFalse(fut.done())
+        await self.send_data(b'{"parameters":{"result":"egg"}}\0')
+        self.assertEqual(await fut, {"result": "egg"})
+        with self.assertRaises(StopAsyncIteration):
+            await anext(gen)

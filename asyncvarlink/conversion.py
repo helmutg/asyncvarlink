helmutg/asyncvarlink

commit 6050794c49400f71c66ea9569203998356befb17

support dataclasses in type conversion

asyncvarlink/conversion.py
@@ -4,6 +4,7 @@
 """Helper for converting between Python objects and JSONValues."""
 
 import contextlib
+import dataclasses
 import enum
 import types
 import typing
@@ -111,6 +112,10 @@ class VarlinkType:
                         for name, elemtype in tobj.__annotations__.items()
                     },
                 )
+            if dataclasses.is_dataclass(tobj):
+                # is_dataclass also returns True for instances.
+                assert isinstance(tobj, type)
+                return DataclassVarlinkType(tobj)
         elif origin is typing.Literal:
             return LiteralVarlinkType(set(args))
         elif origin is typing.Union or origin is types.UnionType:
@@ -433,6 +438,67 @@ class ObjectVarlinkType(VarlinkType):
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}({self._typemap!r})"
+
+
+class DataclassVarlinkType(VarlinkType):
+    """A varlink type representing an object as a dataclasses.dataclass.
+    Only fields with init=True will be considered."""
+
+    def __init__(self, dataclasstype: type):
+        assert dataclasses.is_dataclass(dataclasstype)
+        self.as_type = dataclasstype
+        self.as_varlink = dataclasstype.__name__
+        self._typemap = {
+            field.name: VarlinkType.from_type_annotation(field.type)
+            for field in dataclasses.fields(dataclasstype)
+            if field.init
+        }
+        self.typedefs = {
+            dataclasstype.__name__: "(%s)"
+            % ", ".join(
+                f"{name}: {vtype.as_varlink}"
+                for name, vtype in self._typemap.items()
+            ),
+        }
+        _merge_typedefs(
+            self.typedefs,
+            *(vtype.typedefs for vtype in self._typemap.values()),
+        )
+
+    def tojson(
+        self, obj: typing.Any, oobstate: OOBTypeState = None
+    ) -> JSONObject:
+        if not dataclasses.is_dataclass(obj):
+            raise ConversionError.expected("a dataclass", obj)
+        result = {}
+        for name, vtype in self._typemap.items():
+            with ConversionError.context(name):
+                result[name] = vtype.tojson(getattr(obj, name), oobstate)
+        return result
+
+    def fromjson(
+        self, obj: JSONValue, oobstate: OOBTypeState = None
+    ) -> typing.Any:
+        if not isinstance(obj, dict):
+            raise ConversionError.expected("map", obj)
+        excess_keys = set(obj.keys())
+        excess_keys.difference_update(self._typemap.keys())
+        if excess_keys:
+            raise ConversionError(
+                "unexpected dataclass fields " + ", ".join(excess_keys)
+            )
+        fields = {}
+        for name, vtype in self._typemap.items():
+            try:
+                value = obj[name]
+            except KeyError as err:
+                raise ConversionError(f"missing object key {name}") from err
+            with ConversionError.context(name):
+                fields[name] = vtype.fromjson(value, oobstate)
+        return self.as_type(**fields)
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self.as_type})"
 
 
 class EnumVarlinkType(VarlinkType):

tests/test_conversion.py
@@ -1,6 +1,7 @@
 # Copyright 2024 Helmut Grohne <helmut@subdivi.de>
 # SPDX-License-Identifier: GPL-2+
 
+import dataclasses
 import enum
 import typing
 import unittest
@@ -10,6 +11,7 @@ import hypothesis.strategies as st
 
 from asyncvarlink import (
     ConversionError,
+    DataclassVarlinkType,
     DictVarlinkType,
     EnumVarlinkType,
     FileDescriptor,
@@ -25,6 +27,12 @@ from asyncvarlink import (
     SimpleVarlinkType,
     VarlinkType,
 )
+
+
+@dataclasses.dataclass
+class Quantity:
+    value: float
+    unit: str
 
 
 class TriState(enum.Enum):
@@ -57,6 +65,7 @@ def type_annotations() -> st.SearchStrategy:
         st.just(int),
         st.just(FileDescriptor),
         st.just(float),
+        st.just(Quantity),
         st.just(str),
         st.just(TriState),
         st.just(typing.Literal["on", "off"]),
@@ -115,6 +124,9 @@ def representable(vt: VarlinkType) -> st.SearchStrategy:
                 if not isinstance(value, OptionalVarlinkType)
             },
         )
+    if isinstance(vt, DataclassVarlinkType):
+        assert vt.as_type is Quantity
+        return st.from_type(Quantity)
     assert isinstance(vt, ForeignVarlinkType)
     return st.just(object())
 

commit fc67ffacbbef06d3d4092d3f4a0cb50d369648f4

DataclassVarlinkType: provide type definitions in a more useful order
If we put the dataclass definition first, the definitions used inside
will added later and appear undefined to some parsers.

Fixes: 6050794c4940 ("support dataclasses in type conversion")

asyncvarlink/conversion.py
@@ -453,16 +453,17 @@ class DataclassVarlinkType(VarlinkType):
             for field in dataclasses.fields(dataclasstype)
             if field.init
         }
-        self.typedefs = {
-            dataclasstype.__name__: "(%s)"
-            % ", ".join(
-                f"{name}: {vtype.as_varlink}"
-                for name, vtype in self._typemap.items()
-            ),
-        }
+        self.typedefs = {}
         _merge_typedefs(
             self.typedefs,
             *(vtype.typedefs for vtype in self._typemap.values()),
+            {
+                dataclasstype.__name__: "(%s)"
+                % ", ".join(
+                    f"{name}: {vtype.as_varlink}"
+                    for name, vtype in self._typemap.items()
+                ),
+            },
         )
 
     def tojson(
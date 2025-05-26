helmutg/asyncvarlink

commit 4a2348e1bbf224e10fa8e3898f760624754e5a69

support converting typing.Literal as varlink enum

asyncvarlink/conversion.py
@@ -111,6 +111,8 @@ class VarlinkType:
                         for name in tobj.__optional_keys__
                     },
                 )
+        elif origin is typing.Literal:
+            return LiteralVarlinkType(set(args))
         elif origin is typing.Union or origin is types.UnionType:
             if any(arg is types.NoneType for arg in args):
                 remaining = [alt for alt in args if alt is not types.NoneType]
@@ -420,7 +422,7 @@ class ObjectVarlinkType(VarlinkType):
 
 
 class EnumVarlinkType(VarlinkType):
-    """A varlink type representing an enum/enum.Enum."""
+    """A varlink type representing an enum represented as enum.Enum."""
 
     def __init__(self, enumtype: type[enum.Enum]) -> None:
         if not issubclass(enumtype, enum.Enum):
@@ -448,6 +450,33 @@ class EnumVarlinkType(VarlinkType):
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}({self.as_type!r})"
+
+
+class LiteralVarlinkType(VarlinkType):
+    """A varlink type representation for an enum represented as
+    typing.Literal.
+    """
+
+    def __init__(self, values: set[str]):
+        self._values = values
+        # mypy cannot handle dynamic literals
+        self.as_type = typing.Literal[tuple[values]]  # type: ignore[valid-type]
+        self.as_varlink = "(%s)" % ", ".join(values)
+
+    def tojson(self, obj: typing.Any, oobstate: OOBTypeState = None) -> str:
+        if not (isinstance(obj, str) and obj in self._values):
+            raise ConversionError(f"invalid literal value {obj!r}")
+        return obj
+
+    def fromjson(
+        self, obj: JSONValue, oobstate: OOBTypeState = None
+    ) -> typing.Any:
+        if not (isinstance(obj, str) and obj in self._values):
+            raise ConversionError(f"invalid literal value {obj!r}")
+        return obj
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self._values!r})"
 
 
 class FileDescriptorVarlinkType(VarlinkType):

tests/test_conversion.py
@@ -18,6 +18,7 @@ from asyncvarlink import (
     ForeignVarlinkType,
     JSONValue,
     ListVarlinkType,
+    LiteralVarlinkType,
     ObjectVarlinkType,
     OptionalVarlinkType,
     SetVarlinkType,
@@ -58,6 +59,7 @@ def type_annotations() -> st.SearchStrategy:
         st.just(float),
         st.just(str),
         st.just(TriState),
+        st.just(typing.Literal["on", "off"]),
         st.just(Digits),
         st.just(set[str]),
         st.builds(
@@ -92,6 +94,8 @@ def representable(vt: VarlinkType) -> st.SearchStrategy:
         if vt.as_type == float:
             return st.floats(allow_nan=False)
         return st.from_type(vt.as_type)
+    if isinstance(vt, LiteralVarlinkType):
+        return st.sampled_from(sorted(vt._values))
     if isinstance(vt, FileDescriptorVarlinkType):
         return st.builds(MockedFd, st.integers(min_value=0))
     if isinstance(vt, OptionalVarlinkType):

commit c00fbdbacae5e29781b54e027384efc6c2e5147f

LiteralVarlinkType: convey values as tuple
The previously used set does not preserve order to the point where the
order would change from one invocation to the next due to random hash
seeding. We want a stable order, so using the one from the Literal
sounds good.

Fixes: 4a2348e1bbf2 (" support converting typing.Literal as varlink enum")

asyncvarlink/conversion.py
@@ -117,7 +117,7 @@ class VarlinkType:
                 assert isinstance(tobj, type)
                 return DataclassVarlinkType(tobj)
         elif origin is typing.Literal:
-            return LiteralVarlinkType(set(args))
+            return LiteralVarlinkType(args)
         elif origin is typing.Union or origin is types.UnionType:
             if any(arg is types.NoneType for arg in args):
                 remaining = [alt for alt in args if alt is not types.NoneType]
@@ -540,10 +540,10 @@ class LiteralVarlinkType(VarlinkType):
     typing.Literal.
     """
 
-    def __init__(self, values: set[str]):
+    def __init__(self, values: tuple[str, ...]):
         self._values = values
         # mypy cannot handle dynamic literals
-        self.as_type = typing.Literal[tuple(values)]
+        self.as_type = typing.Literal[values]
         self.as_varlink = "(%s)" % ", ".join(values)
 
     def tojson(self, obj: typing.Any, oobstate: OOBTypeState = None) -> str:
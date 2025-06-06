helmutg/asyncvarlink

commit 425bd7c44e49c8fc0ebc1a645245e040ea4874ef

add a VarlinkServerProtocol class
It glues together the types from the message and error modules with the
protocol class.

asyncvarlink/serverprotocol.py
@@ -1,0 +1,55 @@
+# Copyright 2024 Helmut Grohne <helmut@subdivi.de>
+# SPDX-License-Identifier: GPL-3
+
+"""asyncio varlink server protocol implementation"""
+
+import asyncio
+
+from .error import VarlinkErrorReply, GenericVarlinkErrorReply
+from .message import VarlinkMethodCall, VarlinkMethodReply
+from .protocol import VarlinkProtocol
+from .types import JSONObject, OwnedFileDescriptors
+
+
+class VarlinkServerProtocol(VarlinkProtocol):
+    """Protocol class for a varlink service. It receives calls as
+    VarlinkMethodCall objects and issues replies as VarlinkMethodReply or
+    VarlinkErrorReply objects. A derived class should implement call_received.
+    """
+
+    def send_reply(
+        self,
+        reply: VarlinkMethodReply | VarlinkErrorReply,
+        fds: list[int] | None = None,
+        autoclose: bool = True,
+    ) -> asyncio.Future[None]:
+        """Enqueue the given reply and file descriptors for sending. For the
+        semantics regarding fds, please refer to the documentation of
+        send_message.
+        """
+        return self.send_message(reply.tojson(), fds, autoclose)
+
+    def request_received(
+        self, obj: JSONObject, fds: OwnedFileDescriptors
+    ) -> asyncio.Future[None] | None:
+        try:
+            try:
+                call = VarlinkMethodCall.fromjson(obj)
+            except (TypeError, ValueError):
+                raise GenericVarlinkErrorReply("ProtocolViolation") from None
+            return self.call_received(call, fds)
+        except VarlinkErrorReply as err:
+            if not obj.get("oneway", False):
+                self.send_reply(err)
+            return None
+
+    def call_received(
+        self, call: VarlinkMethodCall, fds: OwnedFileDescriptors
+    ) -> asyncio.Future[None] | None:
+        """Handle a received varlink parsed as a call object and associated
+        file descriptors. The descriptors are valid until the function returns
+        None or the returned future is complete. The function should call the
+        send_reply method as needed or raise a VarlinkErrorReply to be sent by
+        the caller.
+        """
+        raise NotImplementedError

asyncvarlink/__init__.py
@@ -17,5 +17,6 @@ from .error import *
 from .interface import *
 from .message import *
 from .protocol import *
+from .serverprotocol import *
 from .types import *
 from .util import *

commit d8f4c9c10d8082d568f8949cd7ae2c350492efa4

VarlinkServerProtocol: fix replying to protocol errors
We validate names and "ProtocolViolation" happens to not pass
verification causing the construction of the GenericVarlinkErrorReply to
fail. Until we have a proper interface for it, use invalid.asyncvarlink.

Fixes: 425bd7c44e49 ("add a VarlinkServerProtocol class")

asyncvarlink/serverprotocol.py
@@ -136,7 +136,9 @@ class VarlinkServerProtocol(VarlinkProtocol):
             try:
                 call = VarlinkMethodCall.fromjson(obj)
             except (TypeError, ValueError):
-                raise GenericVarlinkErrorReply("ProtocolViolation") from None
+                raise GenericVarlinkErrorReply(
+                    "invalid.asyncvarlink.ProtocolViolation"
+                ) from None
             fut = self.call_received(call, fds)
             if fut is None:
                 return None

tests/test_serverprotocol.py
@@ -125,3 +125,9 @@ class ServerTests(unittest.IsolatedAsyncioTestCase):
             b'{"method":"com.example.demo.AsyncAnswer"}',
             b'{"parameters":{"result":42}}',
         )
+
+    async def test_protocol_violation(self) -> None:
+        await self.invoke(
+            b"{}",
+            b'{"error":"invalid.asyncvarlink.ProtocolViolation"}',
+        )
3e464225115b69389e0abedfd7050bbe518064c3
Helmut Grohne <helmut@subdivi.de>
2025-08-13 07:54:10 +0200

    test broken pipe situation on the server side

--- tests/test_serverprotocol.py
+++ tests/test_serverprotocol.py
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: LGPL-2.0-or-later
 
 import asyncio
+import contextlib
 import json
 import socket
 import typing
@@ -58,7 +59,10 @@ class ServerTests(unittest.IsolatedAsyncioTestCase):
         self.registry = VarlinkInterfaceRegistry()
         self.registry.register_interface(DemoInterface())
 
-    async def invoke(self, request: bytes, expected_response: bytes) -> None:
+    @contextlib.asynccontextmanager
+    async def connected_server(
+        self,
+    ) -> typing.AsyncIterator[tuple[socket.socket, socket.socket]]:
         loop = asyncio.get_running_loop()
         sock1, sock2 = socket.socketpair(
             type=socket.SOCK_STREAM | socket.SOCK_NONBLOCK
@@ -71,11 +75,7 @@ class ServerTests(unittest.IsolatedAsyncioTestCase):
                 sock2,
                 VarlinkInterfaceServerProtocol(self.registry),
             )
-            await loop.sock_sendall(sock1, request + b"\0")
-            data = await loop.sock_recv(sock1, 1024)
-            if json.loads(data.split(b"\0", 1)[0]).get("continues"):
-                data += await loop.sock_recv(sock1, 1024)
-            self.assertEqual(data, expected_response + b"\0")
+            yield (sock1, sock2)
         finally:
             if transport:
                 transport.close()
@@ -84,6 +84,15 @@ class ServerTests(unittest.IsolatedAsyncioTestCase):
             else:
                 sock2.close()
             sock1.close()
+
+    async def invoke(self, request: bytes, expected_response: bytes) -> None:
+        loop = asyncio.get_running_loop()
+        async with self.connected_server() as (sock1, _):
+            await loop.sock_sendall(sock1, request + b"\0")
+            data = await loop.sock_recv(sock1, 1024)
+            if json.loads(data.split(b"\0", 1)[0]).get("continues"):
+                data += await loop.sock_recv(sock1, 1024)
+            self.assertEqual(data, expected_response + b"\0")
 
     async def test_sync_single(self) -> None:
         await self.invoke(
@@ -131,3 +140,18 @@ class ServerTests(unittest.IsolatedAsyncioTestCase):
             b"{}",
             b'{"error":"invalid.asyncvarlink.ProtocolViolation"}',
         )
+
+    async def test_broken_pipe(self) -> None:
+        loop = asyncio.get_running_loop()
+        async with self.connected_server() as (sock1, sock2):
+            await loop.sock_sendall(
+                sock1, b'{"method":"com.example.demo.AsyncAnswer"}\0'
+            )
+            sock1.close()
+            self.assertGreaterEqual(sock2.fileno(), 0)
+            # A single sleep(0) does not seem to suffice here.
+            for _ in range(99):
+                if sock2.fileno() < 0:
+                    break
+                await asyncio.sleep(0)
+            self.assertLess(sock2.fileno(), 0)

helmutg/asyncvarlink

commit 15c76280524885da418fb5d485795a4f06aed57f

move error classes for org.varlink.service to a separate module
The VarlinkInterfaceRegistry depends on them, but it should not be in
the serviceinterface module. Moving the errors is a prerequisite for
splitting the serviceinterface module.

asyncvarlink/serviceinterface.py
@@ -13,6 +13,7 @@ from .interface import (
     varlinkmethod,
     varlinksignature,
 )
+from .serviceerrors import *
 
 
 class VarlinkInterfaceRegistry:
@@ -108,40 +109,3 @@ class VarlinkServiceInterface(VarlinkInterface, name="org.varlink.service"):
         except KeyError:
             raise InterfaceNotFound(interface=interface) from None
         return iface.render_interface_description()
-
-
-class InterfaceNotFound(
-    TypedVarlinkErrorReply, interface="org.varlink.service"
-):
-    """A method was called for an interface that is not provided or a
-    method was called with an interface parameter and said interface is not
-    provided.
-    """
-
-    class Parameters:
-        interface: str
-
-
-class MethodNotFound(TypedVarlinkErrorReply, interface="org.varlink.service"):
-    """A method was called that is not provided on the named interface."""
-
-    class Parameters:
-        method: str
-
-
-class InvalidParameter(
-    TypedVarlinkErrorReply, interface="org.varlink.service"
-):
-    """A call parameter could not be validated to the expected type."""
-
-    class Parameters:
-        parameter: str
-
-
-class ExpectedMore(TypedVarlinkErrorReply, interface="org.varlink.service"):
-    """A method was expecting to be called with "more": true, but was called
-    without.
-    """
-
-    class Parameters:
-        pass

asyncvarlink/serviceerrors.py
@@ -1,0 +1,43 @@
+# Copyright 2024 Helmut Grohne <helmut@subdivi.de>
+# SPDX-License-Identifier: GPL-3
+
+"""Python classes for errors of the org.varlink.service interface."""
+
+from .error import TypedVarlinkErrorReply
+
+
+class InterfaceNotFound(
+    TypedVarlinkErrorReply, interface="org.varlink.service"
+):
+    """A method was called for an interface that is not provided or a
+    method was called with an interface parameter and said interface is not
+    provided.
+    """
+
+    class Parameters:
+        interface: str
+
+
+class MethodNotFound(TypedVarlinkErrorReply, interface="org.varlink.service"):
+    """A method was called that is not provided on the named interface."""
+
+    class Parameters:
+        method: str
+
+
+class InvalidParameter(
+    TypedVarlinkErrorReply, interface="org.varlink.service"
+):
+    """A call parameter could not be validated to the expected type."""
+
+    class Parameters:
+        parameter: str
+
+
+class ExpectedMore(TypedVarlinkErrorReply, interface="org.varlink.service"):
+    """A method was expecting to be called with "more": true, but was called
+    without.
+    """
+
+    class Parameters:
+        pass

commit aa95cfd82f87bbf7645d6f0c5fcc0dc44942d87a

asyncvarlink.serviceinterface: reexport all errors
The .serviceerrors module is supposed to be an implementation detail.
They should all be importable from .serviceinterface as they used to be.

Fixes: 15c762805248 ("move error classes for org.varlink.service to a separate module")

asyncvarlink/serviceinterface.py
@@ -15,6 +15,8 @@ from .serviceerrors import (
     InterfaceNotFound,
     InvalidParameter,
     MethodNotFound,
+    MethodNotImplemented,
+    PermissionDenied,
 )
 
 
@@ -23,6 +25,8 @@ __all__ = [
     "InterfaceNotFound",
     "InvalidParameter",
     "MethodNotFound",
+    "MethodNotImplemented",
+    "PermissionDenied",
     "VarlinkServiceInterface",
 ]
 
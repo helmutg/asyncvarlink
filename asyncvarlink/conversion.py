helmutg/asyncvarlink

commit 9ddf91e0b6468637e80120c552cd299b7d14bb23

employ typing.override decorator

asyncvarlink/clientprotocol.py
@@ -162,6 +162,7 @@ class VarlinkClientProtocol(VarlinkProtocol):
             pending.consumer_done()
             await sendfut
 
+    @typing.override
     def request_received(
         self, obj: JSONObject, fds: FileDescriptorArray | None
     ) -> None:
@@ -171,6 +172,7 @@ class VarlinkClientProtocol(VarlinkProtocol):
             pending = self._pending.popleft()
         pending.request_received(obj, fds)
 
+    @typing.override
     def error_received(
         self,
         err: Exception,
@@ -179,6 +181,7 @@ class VarlinkClientProtocol(VarlinkProtocol):
     ) -> None:
         self.connection_lost(err)
 
+    @typing.override
     def connection_lost(self, exc: Exception | None) -> None:
         super().connection_lost(exc)
         if exc is None:
@@ -188,6 +191,7 @@ class VarlinkClientProtocol(VarlinkProtocol):
             pending.connection_lost(exc)
         self.close()
 
+    @typing.override
     def eof_received(self) -> None:
         super().eof_received()
         self.connection_lost(ConnectionResetError("remote closed connection"))

asyncvarlink/conversion.py
@@ -193,6 +193,7 @@ class SimpleVarlinkType(VarlinkType):
         self.as_varlink = varlinktype
         self._convertible = tuple(convertible)
 
+    @typing.override
     def tojson(
         self, obj: typing.Any, oobstate: OOBTypeState = None
     ) -> JSONValue:
@@ -208,6 +209,7 @@ class SimpleVarlinkType(VarlinkType):
                 ) from exc
         raise ConversionError.expected(self.as_varlink, obj)
 
+    @typing.override
     def fromjson(
         self, obj: JSONValue, oobstate: OOBTypeState = None
     ) -> typing.Any:
@@ -243,6 +245,7 @@ class OptionalVarlinkType(VarlinkType):
         self.typedefs = vtype.typedefs
         self.contains_fds = vtype.contains_fds
 
+    @typing.override
     def tojson(
         self, obj: typing.Any, oobstate: OOBTypeState = None
     ) -> JSONValue:
@@ -250,6 +253,7 @@ class OptionalVarlinkType(VarlinkType):
             return None
         return self._vtype.tojson(obj, oobstate)
 
+    @typing.override
     def fromjson(
         self, obj: JSONValue, oobstate: OOBTypeState = None
     ) -> typing.Any:
@@ -260,6 +264,7 @@ class OptionalVarlinkType(VarlinkType):
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}({self._vtype!r})"
 
+    @typing.override
     def optional(self) -> typing.Self:
         return self
 
@@ -275,6 +280,7 @@ class ListVarlinkType(VarlinkType):
         self.typedefs = elttype.typedefs
         self.contains_fds = elttype.contains_fds
 
+    @typing.override
     def tojson(
         self, obj: typing.Any, oobstate: OOBTypeState = None
     ) -> list[JSONValue]:
@@ -286,6 +292,7 @@ class ListVarlinkType(VarlinkType):
                 result.append(self._elttype.tojson(elt, oobstate))
         return result
 
+    @typing.override
     def fromjson(
         self, obj: JSONValue, oobstate: OOBTypeState = None
     ) -> typing.Any:
@@ -314,6 +321,7 @@ class DictVarlinkType(VarlinkType):
         self.typedefs = elttype.typedefs
         self.contains_fds = elttype.contains_fds
 
+    @typing.override
     def tojson(
         self, obj: typing.Any, oobstate: OOBTypeState = None
     ) -> JSONObject:
@@ -327,6 +335,7 @@ class DictVarlinkType(VarlinkType):
                 result[key] = self._elttype.tojson(value, oobstate)
         return result
 
+    @typing.override
     def fromjson(
         self, obj: JSONValue, oobstate: OOBTypeState = None
     ) -> typing.Any:
@@ -353,6 +362,7 @@ class SetVarlinkType(VarlinkType):
     as_varlink = "[string]()"
     contains_fds = False
 
+    @typing.override
     def tojson(
         self, obj: typing.Any, oobstate: OOBTypeState = None
     ) -> JSONObject:
@@ -366,6 +376,7 @@ class SetVarlinkType(VarlinkType):
             result[elem] = {}
         return result
 
+    @typing.override
     def fromjson(
         self, obj: JSONValue, oobstate: OOBTypeState = None
     ) -> typing.Any:
@@ -411,6 +422,7 @@ class ObjectVarlinkType(VarlinkType):
         )
         self.contains_fds = any(tobj.contains_fds for tobj in typemap.values())
 
+    @typing.override
     def tojson(
         self, obj: typing.Any, oobstate: OOBTypeState = None
     ) -> JSONObject:
@@ -433,6 +445,7 @@ class ObjectVarlinkType(VarlinkType):
                 raise ConversionError(f"no type for key {key}", [key])
         return result
 
+    @typing.override
     def fromjson(
         self, obj: JSONValue, oobstate: OOBTypeState = None
     ) -> typing.Any:
@@ -488,6 +501,7 @@ class DataclassVarlinkType(VarlinkType):
             tobj.contains_fds for tobj in self._typemap.values()
         )
 
+    @typing.override
     def tojson(
         self, obj: typing.Any, oobstate: OOBTypeState = None
     ) -> JSONObject:
@@ -499,6 +513,7 @@ class DataclassVarlinkType(VarlinkType):
                 result[name] = vtype.tojson(getattr(obj, name), oobstate)
         return result
 
+    @typing.override
     def fromjson(
         self, obj: JSONValue, oobstate: OOBTypeState = None
     ) -> typing.Any:
@@ -542,12 +557,14 @@ class EnumVarlinkType(VarlinkType):
             enumtype.__name__: "(%s)" % ", ".join(enumtype.__members__)
         }
 
+    @typing.override
     def tojson(self, obj: typing.Any, oobstate: OOBTypeState = None) -> str:
         if not isinstance(obj, self.as_type):
             raise ConversionError.expected(f"enum {self.as_type!r}", obj)
         assert isinstance(obj, enum.Enum)
         return obj.name
 
+    @typing.override
     def fromjson(
         self, obj: JSONValue, oobstate: OOBTypeState = None
     ) -> typing.Any:
@@ -577,11 +594,13 @@ class LiteralVarlinkType(VarlinkType):
         self.as_type = typing.Literal[values]
         self.as_varlink = "(%s)" % ", ".join(values)
 
+    @typing.override
     def tojson(self, obj: typing.Any, oobstate: OOBTypeState = None) -> str:
         if not (isinstance(obj, str) and obj in self._values):
             raise ConversionError(f"invalid literal value {obj!r}")
         return obj
 
+    @typing.override
     def fromjson(
         self, obj: JSONValue, oobstate: OOBTypeState = None
     ) -> typing.Any:
@@ -616,6 +635,7 @@ class FileDescriptorVarlinkType(VarlinkType):
                 "cannot convert a file descriptor without associated oobstate"
             ) from None
 
+    @typing.override
     def tojson(self, obj: typing.Any, oobstate: OOBTypeState = None) -> int:
         """Represent a file descriptor. It may be conveyed as int | HasFileno.
         The actual file descriptor is appended to the out-of-band state array
@@ -636,6 +656,7 @@ class FileDescriptorVarlinkType(VarlinkType):
             raise ConversionError("invalid file descriptor") from err
         return index
 
+    @typing.override
     def fromjson(
         self, obj: JSONValue, oobstate: OOBTypeState = None
     ) -> typing.Any:
@@ -679,6 +700,7 @@ class ForeignVarlinkType(VarlinkType):
     def __init__(self, varlink: str = "object"):
         self.as_varlink = varlink
 
+    @typing.override
     def tojson(
         self, obj: typing.Any, oobstate: OOBTypeState = None
     ) -> JSONValue:
@@ -686,6 +708,7 @@ class ForeignVarlinkType(VarlinkType):
         # hope that the user is doing things correctly.
         return typing.cast(JSONValue, obj)
 
+    @typing.override
     def fromjson(
         self, obj: JSONValue, oobstate: OOBTypeState = None
     ) -> typing.Any:

asyncvarlink/error.py
@@ -63,6 +63,7 @@ class GenericVarlinkErrorReply(VarlinkErrorReply):
         self.name = error
         self.parameters = {} if parameters is None else parameters
 
+    @typing.override
     def paramstojson(self) -> JSONObject:
         return self.parameters
 
@@ -184,6 +185,7 @@ class TypedVarlinkErrorReply(VarlinkErrorReply):
             setattr(cls, descname, desc)
             cls.__annotations__[descname] = ann
 
+    @typing.override
     def paramstojson(self) -> JSONObject:
         ret = self.paramtype.tojson(self.parameters)
         assert isinstance(ret, dict)  # self.parameters is a dict.

asyncvarlink/protocol.py
@@ -105,10 +105,12 @@ class VarlinkTransport(asyncio.BaseTransport):
         self._loop.call_soon(self._protocol.connection_made, self)
         self._loop.call_soon(self.resume_receiving)
 
+    @typing.override
     def set_protocol(self, protocol: asyncio.BaseProtocol) -> None:
         assert isinstance(protocol, VarlinkBaseProtocol)
         self._protocol = protocol
 
+    @typing.override
     def get_protocol(self) -> "VarlinkBaseProtocol":
         return self._protocol
 
@@ -308,11 +310,13 @@ class VarlinkTransport(asyncio.BaseTransport):
             if not self._sendqueue:
                 self._close_sender()
 
+    @typing.override
     def close(self) -> None:
         if not self._closing:
             self._closing = True
             self._loop.call_soon(self._connection_lost)
 
+    @typing.override
     def is_closing(self) -> bool:
         return self._closing
 
@@ -372,10 +376,12 @@ class VarlinkProtocol(VarlinkBaseProtocol):
         ] = collections.deque()
         self._transport: VarlinkTransport | None = None
 
+    @typing.override
     def connection_made(self, transport: asyncio.BaseTransport) -> None:
         assert isinstance(transport, VarlinkTransport)
         self._transport = transport
 
+    @typing.override
     def message_received(
         self, data: bytes, fds: FileDescriptorArray | None
     ) -> None:

asyncvarlink/serverprotocol.py
@@ -161,6 +161,7 @@ class VarlinkServerProtocol(VarlinkProtocol):
                 )
             )
 
+    @typing.override
     def request_received(
         self, obj: JSONObject, fds: FileDescriptorArray | None
     ) -> asyncio.Future[None] | None:
@@ -218,6 +219,7 @@ class VarlinkInterfaceServerProtocol(VarlinkServerProtocol):
         super().__init__()
         self._registry = registry
 
+    @typing.override
     def call_received(
         self, call: VarlinkMethodCall, fds: FileDescriptorArray | None
     ) -> asyncio.Future[None] | None:

asyncvarlink/types.py
@@ -338,6 +338,7 @@ class FileDescriptorArray(FutureCounted):
     def __iter__(self) -> typing.Iterator[FileDescriptor]:
         return iter(self._by_position)
 
+    @typing.override
     def destroy(self) -> None:
         """Release all file descriptors. This amounts to closing owned ones.
         Idempotent.

asyncvarlink/util.py
@@ -99,18 +99,22 @@ class VarlinkUnixServer(asyncio.AbstractServer):
         self._serving: asyncio.Future[None] | None = None
         self._transports: weakref.WeakSet[VarlinkTransport] = weakref.WeakSet()
 
+    @typing.override
     def close(self) -> None:
         if self._serving is not None:
             self._loop.remove_reader(self._sock)
             self._serving.set_result(None)
             self._serving = None
 
+    @typing.override
     def get_loop(self) -> asyncio.AbstractEventLoop:
         return self._loop
 
+    @typing.override
     def is_serving(self) -> bool:
         return self._serving is not None
 
+    @typing.override
     async def start_serving(self) -> None:
         if self._serving is None:
             self._serving = self._loop.create_future()
@@ -136,19 +140,23 @@ class VarlinkUnixServer(asyncio.AbstractServer):
             )
         )
 
+    @typing.override
     async def serve_forever(self) -> None:
         await self.start_serving()
         await self.wait_closed()
 
+    @typing.override
     async def wait_closed(self) -> None:
         if self._serving is None:
             return
         await self._serving
 
+    @typing.override
     def close_clients(self) -> None:
         for transport in self._transports.copy():
             transport.close()
 
+    @typing.override
     def abort_clients(self) -> None:
         for transport in self._transports.copy():
             transport.abort()

commit 256092a66a0156004281b597f3b29932875e1456

support Python 3.11 when using typing.override
Fixes: 9ddf91e0b646 ("employ typing.override decorator")

asyncvarlink/clientprotocol.py
@@ -13,7 +13,7 @@ from .conversion import FileDescriptorVarlinkType
 from .interface import VarlinkInterface, varlinksignature
 from .message import VarlinkMethodCall, VarlinkMethodReply
 from .protocol import VarlinkProtocol
-from .types import FileDescriptorArray, JSONObject
+from .types import FileDescriptorArray, JSONObject, override
 from .util import completing_future
 
 
@@ -162,7 +162,7 @@ class VarlinkClientProtocol(VarlinkProtocol):
             pending.consumer_done()
             await sendfut
 
-    @typing.override
+    @override
     def request_received(
         self, obj: JSONObject, fds: FileDescriptorArray | None
     ) -> None:
@@ -172,7 +172,7 @@ class VarlinkClientProtocol(VarlinkProtocol):
             pending = self._pending.popleft()
         pending.request_received(obj, fds)
 
-    @typing.override
+    @override
     def error_received(
         self,
         err: Exception,
@@ -181,7 +181,7 @@ class VarlinkClientProtocol(VarlinkProtocol):
     ) -> None:
         self.connection_lost(err)
 
-    @typing.override
+    @override
     def connection_lost(self, exc: Exception | None) -> None:
         super().connection_lost(exc)
         if exc is None:
@@ -191,7 +191,7 @@ class VarlinkClientProtocol(VarlinkProtocol):
             pending.connection_lost(exc)
         self.close()
 
-    @typing.override
+    @override
     def eof_received(self) -> None:
         super().eof_received()
         self.connection_lost(ConnectionResetError("remote closed connection"))

asyncvarlink/conversion.py
@@ -9,7 +9,13 @@ import enum
 import types
 import typing
 
-from .types import FileDescriptor, FileDescriptorArray, JSONValue, JSONObject
+from .types import (
+    FileDescriptor,
+    FileDescriptorArray,
+    JSONValue,
+    JSONObject,
+    override,
+)
 
 
 class ConversionError(Exception):
@@ -193,7 +199,7 @@ class SimpleVarlinkType(VarlinkType):
         self.as_varlink = varlinktype
         self._convertible = tuple(convertible)
 
-    @typing.override
+    @override
     def tojson(
         self, obj: typing.Any, oobstate: OOBTypeState = None
     ) -> JSONValue:
@@ -209,7 +215,7 @@ class SimpleVarlinkType(VarlinkType):
                 ) from exc
         raise ConversionError.expected(self.as_varlink, obj)
 
-    @typing.override
+    @override
     def fromjson(
         self, obj: JSONValue, oobstate: OOBTypeState = None
     ) -> typing.Any:
@@ -245,7 +251,7 @@ class OptionalVarlinkType(VarlinkType):
         self.typedefs = vtype.typedefs
         self.contains_fds = vtype.contains_fds
 
-    @typing.override
+    @override
     def tojson(
         self, obj: typing.Any, oobstate: OOBTypeState = None
     ) -> JSONValue:
@@ -253,7 +259,7 @@ class OptionalVarlinkType(VarlinkType):
             return None
         return self._vtype.tojson(obj, oobstate)
 
-    @typing.override
+    @override
     def fromjson(
         self, obj: JSONValue, oobstate: OOBTypeState = None
     ) -> typing.Any:
@@ -264,7 +270,7 @@ class OptionalVarlinkType(VarlinkType):
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}({self._vtype!r})"
 
-    @typing.override
+    @override
     def optional(self) -> typing.Self:
         return self
 
@@ -280,7 +286,7 @@ class ListVarlinkType(VarlinkType):
         self.typedefs = elttype.typedefs
         self.contains_fds = elttype.contains_fds
 
-    @typing.override
+    @override
     def tojson(
         self, obj: typing.Any, oobstate: OOBTypeState = None
     ) -> list[JSONValue]:
@@ -292,7 +298,7 @@ class ListVarlinkType(VarlinkType):
                 result.append(self._elttype.tojson(elt, oobstate))
         return result
 
-    @typing.override
+    @override
     def fromjson(
         self, obj: JSONValue, oobstate: OOBTypeState = None
     ) -> typing.Any:
@@ -321,7 +327,7 @@ class DictVarlinkType(VarlinkType):
         self.typedefs = elttype.typedefs
         self.contains_fds = elttype.contains_fds
 
-    @typing.override
+    @override
     def tojson(
         self, obj: typing.Any, oobstate: OOBTypeState = None
     ) -> JSONObject:
@@ -335,7 +341,7 @@ class DictVarlinkType(VarlinkType):
                 result[key] = self._elttype.tojson(value, oobstate)
         return result
 
-    @typing.override
+    @override
     def fromjson(
         self, obj: JSONValue, oobstate: OOBTypeState = None
     ) -> typing.Any:
@@ -362,7 +368,7 @@ class SetVarlinkType(VarlinkType):
     as_varlink = "[string]()"
     contains_fds = False
 
-    @typing.override
+    @override
     def tojson(
         self, obj: typing.Any, oobstate: OOBTypeState = None
     ) -> JSONObject:
@@ -376,7 +382,7 @@ class SetVarlinkType(VarlinkType):
             result[elem] = {}
         return result
 
-    @typing.override
+    @override
     def fromjson(
         self, obj: JSONValue, oobstate: OOBTypeState = None
     ) -> typing.Any:
@@ -422,7 +428,7 @@ class ObjectVarlinkType(VarlinkType):
         )
         self.contains_fds = any(tobj.contains_fds for tobj in typemap.values())
 
-    @typing.override
+    @override
     def tojson(
         self, obj: typing.Any, oobstate: OOBTypeState = None
     ) -> JSONObject:
@@ -445,7 +451,7 @@ class ObjectVarlinkType(VarlinkType):
                 raise ConversionError(f"no type for key {key}", [key])
         return result
 
-    @typing.override
+    @override
     def fromjson(
         self, obj: JSONValue, oobstate: OOBTypeState = None
     ) -> typing.Any:
@@ -501,7 +507,7 @@ class DataclassVarlinkType(VarlinkType):
             tobj.contains_fds for tobj in self._typemap.values()
         )
 
-    @typing.override
+    @override
     def tojson(
         self, obj: typing.Any, oobstate: OOBTypeState = None
     ) -> JSONObject:
@@ -513,7 +519,7 @@ class DataclassVarlinkType(VarlinkType):
                 result[name] = vtype.tojson(getattr(obj, name), oobstate)
         return result
 
-    @typing.override
+    @override
     def fromjson(
         self, obj: JSONValue, oobstate: OOBTypeState = None
     ) -> typing.Any:
@@ -557,14 +563,14 @@ class EnumVarlinkType(VarlinkType):
             enumtype.__name__: "(%s)" % ", ".join(enumtype.__members__)
         }
 
-    @typing.override
+    @override
     def tojson(self, obj: typing.Any, oobstate: OOBTypeState = None) -> str:
         if not isinstance(obj, self.as_type):
             raise ConversionError.expected(f"enum {self.as_type!r}", obj)
         assert isinstance(obj, enum.Enum)
         return obj.name
 
-    @typing.override
+    @override
     def fromjson(
         self, obj: JSONValue, oobstate: OOBTypeState = None
     ) -> typing.Any:
@@ -594,13 +600,13 @@ class LiteralVarlinkType(VarlinkType):
         self.as_type = typing.Literal[values]
         self.as_varlink = "(%s)" % ", ".join(values)
 
-    @typing.override
+    @override
     def tojson(self, obj: typing.Any, oobstate: OOBTypeState = None) -> str:
         if not (isinstance(obj, str) and obj in self._values):
             raise ConversionError(f"invalid literal value {obj!r}")
         return obj
 
-    @typing.override
+    @override
     def fromjson(
         self, obj: JSONValue, oobstate: OOBTypeState = None
     ) -> typing.Any:
@@ -635,7 +641,7 @@ class FileDescriptorVarlinkType(VarlinkType):
                 "cannot convert a file descriptor without associated oobstate"
             ) from None
 
-    @typing.override
+    @override
     def tojson(self, obj: typing.Any, oobstate: OOBTypeState = None) -> int:
         """Represent a file descriptor. It may be conveyed as int | HasFileno.
         The actual file descriptor is appended to the out-of-band state array
@@ -656,7 +662,7 @@ class FileDescriptorVarlinkType(VarlinkType):
             raise ConversionError("invalid file descriptor") from err
         return index
 
-    @typing.override
+    @override
     def fromjson(
         self, obj: JSONValue, oobstate: OOBTypeState = None
     ) -> typing.Any:
@@ -700,7 +706,7 @@ class ForeignVarlinkType(VarlinkType):
     def __init__(self, varlink: str = "object"):
         self.as_varlink = varlink
 
-    @typing.override
+    @override
     def tojson(
         self, obj: typing.Any, oobstate: OOBTypeState = None
     ) -> JSONValue:
@@ -708,7 +714,7 @@ class ForeignVarlinkType(VarlinkType):
         # hope that the user is doing things correctly.
         return typing.cast(JSONValue, obj)
 
-    @typing.override
+    @override
     def fromjson(
         self, obj: JSONValue, oobstate: OOBTypeState = None
     ) -> typing.Any:

asyncvarlink/error.py
@@ -8,7 +8,7 @@ import typing
 
 from .conversion import ObjectVarlinkType, VarlinkType
 from .message import VarlinkMethodReply
-from .types import JSONObject, JSONValue
+from .types import JSONObject, JSONValue, override
 
 
 class _VarlinkErrorParameterDescriptor:
@@ -63,7 +63,7 @@ class GenericVarlinkErrorReply(VarlinkErrorReply):
         self.name = error
         self.parameters = {} if parameters is None else parameters
 
-    @typing.override
+    @override
     def paramstojson(self) -> JSONObject:
         return self.parameters
 
@@ -185,7 +185,7 @@ class TypedVarlinkErrorReply(VarlinkErrorReply):
             setattr(cls, descname, desc)
             cls.__annotations__[descname] = ann
 
-    @typing.override
+    @override
     def paramstojson(self) -> JSONObject:
         ret = self.paramtype.tojson(self.parameters)
         assert isinstance(ret, dict)  # self.parameters is a dict.

asyncvarlink/protocol.py
@@ -19,6 +19,7 @@ from .types import (
     HasFileno,
     JSONObject,
     close_fileno,
+    override,
 )
 
 
@@ -105,12 +106,12 @@ class VarlinkTransport(asyncio.BaseTransport):
         self._loop.call_soon(self._protocol.connection_made, self)
         self._loop.call_soon(self.resume_receiving)
 
-    @typing.override
+    @override
     def set_protocol(self, protocol: asyncio.BaseProtocol) -> None:
         assert isinstance(protocol, VarlinkBaseProtocol)
         self._protocol = protocol
 
-    @typing.override
+    @override
     def get_protocol(self) -> "VarlinkBaseProtocol":
         return self._protocol
 
@@ -310,13 +311,13 @@ class VarlinkTransport(asyncio.BaseTransport):
             if not self._sendqueue:
                 self._close_sender()
 
-    @typing.override
+    @override
     def close(self) -> None:
         if not self._closing:
             self._closing = True
             self._loop.call_soon(self._connection_lost)
 
-    @typing.override
+    @override
     def is_closing(self) -> bool:
         return self._closing
 
@@ -376,12 +377,12 @@ class VarlinkProtocol(VarlinkBaseProtocol):
         ] = collections.deque()
         self._transport: VarlinkTransport | None = None
 
-    @typing.override
+    @override
     def connection_made(self, transport: asyncio.BaseTransport) -> None:
         assert isinstance(transport, VarlinkTransport)
         self._transport = transport
 
-    @typing.override
+    @override
     def message_received(
         self, data: bytes, fds: FileDescriptorArray | None
     ) -> None:

asyncvarlink/serverprotocol.py
@@ -25,7 +25,7 @@ from .serviceerrors import (
     InvalidParameter,
     MethodNotFound,
 )
-from .types import FileDescriptorArray, JSONObject
+from .types import FileDescriptorArray, JSONObject, override
 
 
 _logger = logging.getLogger("asyncvarlink.serverprotocol")
@@ -161,7 +161,7 @@ class VarlinkServerProtocol(VarlinkProtocol):
                 )
             )
 
-    @typing.override
+    @override
     def request_received(
         self, obj: JSONObject, fds: FileDescriptorArray | None
     ) -> asyncio.Future[None] | None:
@@ -219,7 +219,7 @@ class VarlinkInterfaceServerProtocol(VarlinkServerProtocol):
         super().__init__()
         self._registry = registry
 
-    @typing.override
+    @override
     def call_received(
         self, call: VarlinkMethodCall, fds: FileDescriptorArray | None
     ) -> asyncio.Future[None] | None:

asyncvarlink/types.py
@@ -11,6 +11,28 @@ import re
 import typing
 
 
+__all__ = [
+    "FileDescriptor",
+    "FileDescriptor",
+    "FutureCounted",
+    "HasFileno",
+    "JSONObject",
+    "JSONValue",
+    "close_fileno",
+    "override",
+    "validate_interface",
+    "validate_name",
+]
+
+if typing.TYPE_CHECKING:
+    from typing import override
+else:
+
+    def override(func):
+        """typing.override stub to be removed when dropping 3.11 support."""
+        return func
+
+
 _logger_fd = logging.getLogger("asyncvarlink.filedescriptor")
 
 
@@ -338,7 +360,7 @@ class FileDescriptorArray(FutureCounted):
     def __iter__(self) -> typing.Iterator[FileDescriptor]:
         return iter(self._by_position)
 
-    @typing.override
+    @override
     def destroy(self) -> None:
         """Release all file descriptors. This amounts to closing owned ones.
         Idempotent.

asyncvarlink/util.py
@@ -12,7 +12,7 @@ import typing
 import weakref
 
 from .protocol import _BLOCKING_ERRNOS, VarlinkBaseProtocol, VarlinkTransport
-from .types import FileDescriptor
+from .types import FileDescriptor, override
 
 
 _T = typing.TypeVar("_T")
@@ -99,22 +99,22 @@ class VarlinkUnixServer(asyncio.AbstractServer):
         self._serving: asyncio.Future[None] | None = None
         self._transports: weakref.WeakSet[VarlinkTransport] = weakref.WeakSet()
 
-    @typing.override
+    @override
     def close(self) -> None:
         if self._serving is not None:
             self._loop.remove_reader(self._sock)
             self._serving.set_result(None)
             self._serving = None
 
-    @typing.override
+    @override
     def get_loop(self) -> asyncio.AbstractEventLoop:
         return self._loop
 
-    @typing.override
+    @override
     def is_serving(self) -> bool:
         return self._serving is not None
 
-    @typing.override
+    @override
     async def start_serving(self) -> None:
         if self._serving is None:
             self._serving = self._loop.create_future()
@@ -140,23 +140,23 @@ class VarlinkUnixServer(asyncio.AbstractServer):
             )
         )
 
-    @typing.override
+    @override
     async def serve_forever(self) -> None:
         await self.start_serving()
         await self.wait_closed()
 
-    @typing.override
+    @override
     async def wait_closed(self) -> None:
         if self._serving is None:
             return
         await self._serving
 
-    @typing.override
+    @override
     def close_clients(self) -> None:
         for transport in self._transports.copy():
             transport.close()
 
-    @typing.override
+    @override
     def abort_clients(self) -> None:
         for transport in self._transports.copy():
             transport.abort()

tests/test_clientprotocol.py
@@ -11,6 +11,7 @@ import unittest
 from asyncvarlink import (
     ConversionError,
     FileDescriptor,
+    override,
     TypedVarlinkErrorReply,
     VarlinkClientProtocol,
     VarlinkErrorReply,
@@ -44,7 +45,7 @@ class DemoInterface(VarlinkInterface, name="com.example.demo"):
 
 
 class ClientTests(unittest.IsolatedAsyncioTestCase):
-    @typing.override
+    @override
     async def asyncSetUp(self) -> None:
         await super().asyncSetUp()
         self.loop = asyncio.get_running_loop()
@@ -57,7 +58,7 @@ class ClientTests(unittest.IsolatedAsyncioTestCase):
         )
         self.proxy = self.proto.make_proxy(DemoInterface)
 
-    @typing.override
+    @override
     async def asyncTearDown(self) -> None:
         self.transport.close()
         await asyncio.sleep(0)
@@ -235,7 +236,7 @@ class ClientTests(unittest.IsolatedAsyncioTestCase):
 
 
 class ClientPipeTests(unittest.IsolatedAsyncioTestCase):
-    @typing.override
+    @override
     async def asyncSetUp(self) -> None:
         await super().asyncSetUp()
         self.loop = asyncio.get_running_loop()

tests/test_serverprotocol.py
@@ -11,6 +11,7 @@ import unittest
 
 from asyncvarlink import (
     FileDescriptor,
+    override,
     TypedVarlinkErrorReply,
     VarlinkInterface,
     VarlinkInterfaceRegistry,
@@ -71,7 +72,7 @@ class DemoInterface(VarlinkInterface, name="com.example.demo"):
 
 
 class ServerTests(unittest.IsolatedAsyncioTestCase):
-    @typing.override
+    @override
     def setUp(self) -> None:
         super().setUp()
         self.registry = VarlinkInterfaceRegistry()

tests/test_serviceinterface.py
@@ -1,10 +1,9 @@
 # Copyright 2024 Helmut Grohne <helmut@subdivi.de>
 # SPDX-License-Identifier: LGPL-2.0-or-later
 
-import typing
 import unittest
 
-from asyncvarlink import AnnotatedResult, VarlinkInterfaceRegistry
+from asyncvarlink import AnnotatedResult, VarlinkInterfaceRegistry, override
 from asyncvarlink.serviceinterface import (
     InterfaceNotFound,
     VarlinkServiceInterface,
@@ -12,7 +11,7 @@ from asyncvarlink.serviceinterface import (
 
 
 class TestServiceInterface(unittest.TestCase):
-    @typing.override
+    @override
     def setUp(self) -> None:
         self.reg = VarlinkInterfaceRegistry()
         self.vsi = VarlinkServiceInterface(
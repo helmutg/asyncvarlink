helmutg/asyncvarlink

commit 439d69049a02fcce5199d7e4b218ebe22c978bc7

refactor the lifetime management of file descriptor arrays
The life time of OwnedFileDescriptors was previously bound to a returned
future. The same future was used for backpressure. This way of managing
lifetime proved to be difficult to handle, so we now try a different
approach.

The new FileDescriptorArray (renamed) has a quite short lifetime, but a
consumer can extend its lifetime in a way similar to reference counting
- by attaching futures. When passed to a function, an array is collected
when the function returns unless it requests an extension. In cases
where the array is used in call_soon, such an extension becomes
required.

To avoid tedious lifetime management for the common case where there are
no file descriptors, a None value may be passed instead. Even though
this adds a conditional in a few places, it removes the need for
creating quite some futures.

asyncvarlink/conversion.py
@@ -8,7 +8,7 @@ import enum
 import types
 import typing
 
-from .types import FileDescriptor, JSONValue, OwnedFileDescriptors
+from .types import FileDescriptor, FileDescriptorArray, JSONValue
 
 
 class ConversionError(Exception):
@@ -505,7 +505,7 @@ class FileDescriptorVarlinkType(VarlinkType):
         self, obj: JSONValue, oobstate: OOBTypeState = None
     ) -> typing.Any:
         """Unrepresent a file descriptor. The int value is understood as an
-        index into the out-of-band state object of type OwnedFileDescriptors. A
+        index into the out-of-band state object of type FileDescriptorArray. A
         file descriptor is looked up at the index and the position is released
         there.
         """
@@ -515,14 +515,18 @@ class FileDescriptorVarlinkType(VarlinkType):
             raise ConversionError(
                 "cannot unrepresent a file descriptor without oobstate"
             )
-        ownedfds = self._get_oob(oobstate)
-        if not isinstance(ownedfds, OwnedFileDescriptors):
+        fds = self._get_oob(oobstate)
+        if fds is None:
+            raise ConversionError(
+                f"attempt to convert invalid file descriptor index {obj}"
+            )
+        if not isinstance(fds, FileDescriptorArray):
             raise ConversionError(
                 f"out-of-band state for {self.__class__.__name__} should be "
-                f"OwnedFileDescriptors, is {type(ownedfds)}"
+                f"FileDescriptorArray, is {type(fds)}"
             )
         try:
-            return ownedfds.take(obj)
+            return fds.take(obj)
         except IndexError as err:
             raise ConversionError(
                 f"attempt to convert invalid file descriptor index {obj}"

asyncvarlink/protocol.py
@@ -15,13 +15,16 @@ import typing
 
 from .types import (
     FileDescriptor,
+    FileDescriptorArray,
     HasFileno,
     JSONObject,
-    OwnedFileDescriptors,
     close_fileno,
 )
 
 logger = logging.getLogger("asyncvarlink.protocol")
+
+
+SENTINEL = object()
 
 
 def _check_socket(thing: socket.socket | int | HasFileno) -> HasFileno:
@@ -130,19 +133,16 @@ class VarlinkTransport(asyncio.BaseTransport):
             self._loop.remove_reader(self._recvfd)
             self._close_receiver()
             return
-        ownedfds = OwnedFileDescriptors(fds)
         if msg:
+            ownedfds = FileDescriptorArray(SENTINEL, fds) if fds else None
             try:
-                maybefut = self._protocol.message_received(msg, ownedfds)
-            except:
-                ownedfds.close()
-                raise
-            if maybefut:
-                maybefut.add_done_callback(lambda _fut: ownedfds.close())
-            else:
-                ownedfds.close()
+                self._protocol.message_received(msg, ownedfds)
+            finally:
+                if ownedfds:
+                    ownedfds.release(SENTINEL)
         else:
-            ownedfds.close()
+            for fd in fds:
+                os.close(fd)
             self._loop.remove_reader(self._recvfd)
             try:
                 self._protocol.eof_received()
@@ -161,7 +161,7 @@ class VarlinkTransport(asyncio.BaseTransport):
             self._close_receiver()
             return
         if data:
-            self._protocol.message_received(data, OwnedFileDescriptors())
+            self._protocol.message_received(data, None)
         else:
             self._loop.remove_reader(self._recvfd)
             try:
@@ -325,13 +325,13 @@ class VarlinkProtocol(asyncio.BaseProtocol):
         self._transport = transport
 
     def message_received(
-        self, data: bytes, fds: OwnedFileDescriptors
-    ) -> asyncio.Future[None] | None:
+        self, data: bytes, fds: FileDescriptorArray | None
+    ) -> None:
         """Called when the transport received new data. The data can be
-        accompanied by open file descriptors. The caller will close all
-        passed file descriptors that have not been taken
-        (OwnedFileDescriptors.take) once message_received returns None or the
-        returned future completes.
+        accompanied by open file descriptors. The caller will release the fds
+        array and all contained file descriptors that have not been taken
+        (FileDescriptorArray.take) once message_received returns None. Life
+        time of fds can be extended by adding a reference prior to returning.
         """
         parts = data.split(b"\0")
         if self._recv_buffer:
@@ -339,13 +339,11 @@ class VarlinkProtocol(asyncio.BaseProtocol):
         self._recv_buffer = parts.pop()
         loop = asyncio.get_running_loop()
         processing = bool(self._consumer_queue)
-        result = None
         for reqdata in parts:
-            fut = loop.create_future() if fds else None
-            if result is None:
-                result = fut
-            else:
-                assert fut is None
+            fut = None
+            if fds:
+                fut = loop.create_future()
+                fds.reference_until_done(fut)
             try:
                 obj = json.loads(reqdata)
             except json.decoder.JSONDecodeError as err:
@@ -364,8 +362,7 @@ class VarlinkProtocol(asyncio.BaseProtocol):
             if not processing:
                 loop.call_soon(self._process_queue, None)
                 processing = True
-            fds = OwnedFileDescriptors()
-        return result
+            fds = None
 
     def _process_queue(self, _: asyncio.Future[None] | None) -> None:
         assert self._transport is not None
@@ -377,9 +374,9 @@ class VarlinkProtocol(asyncio.BaseProtocol):
         try:
             fut = consume()
         finally:
+            if notify is not None:
+                notify.set_result(None)
             if fut is None or fut.done():
-                if notify is not None:
-                    notify.set_result(None)
                 # If the consumer finishes immediately, skip back pressure
                 # via pause_receiving as that typically incurs two syscalls.
                 if self._consumer_queue:
@@ -389,8 +386,6 @@ class VarlinkProtocol(asyncio.BaseProtocol):
                 else:
                     self._transport.resume_receiving()
             else:
-                if notify is not None:
-                    fut.add_done_callback(lambda _fut: notify.set_result(None))
                 fut.add_done_callback(self._process_queue)
                 self._transport.pause_receiving()
 
@@ -400,19 +395,19 @@ class VarlinkProtocol(asyncio.BaseProtocol):
         """
 
     def request_received(
-        self, obj: JSONObject, fds: OwnedFileDescriptors
+        self, obj: JSONObject, fds: FileDescriptorArray | None
     ) -> asyncio.Future[None] | None:
         """Handle an incoming varlink request or response object together with
         associated file descriptors. If the handler returns a future, further
         processing will be delayed until the future is done. Once the function
-        returns None or the returned future completes all remaining fds that
-        have not been taken (OwnedFileDescriptors.take) will be closed.
+        returns None, the fds will be released unless its life time has been
+        extended by adding another referee.
         """
         raise NotImplementedError
 
     # pylint: disable=unused-argument  # Arguments provided for inheritance
     def error_received(
-        self, err: Exception, data: bytes, fds: OwnedFileDescriptors
+        self, err: Exception, data: bytes, fds: FileDescriptorArray | None
     ) -> None:
         """Handle an incoming protocol violation such as wrongly encoded JSON.
         The default handler does nothing.

asyncvarlink/serverprotocol.py
@@ -8,7 +8,7 @@ import asyncio
 from .error import VarlinkErrorReply, GenericVarlinkErrorReply
 from .message import VarlinkMethodCall, VarlinkMethodReply
 from .protocol import VarlinkProtocol
-from .types import JSONObject, OwnedFileDescriptors
+from .types import FileDescriptorArray, JSONObject
 
 
 class VarlinkServerProtocol(VarlinkProtocol):
@@ -30,7 +30,7 @@ class VarlinkServerProtocol(VarlinkProtocol):
         return self.send_message(reply.tojson(), fds, autoclose)
 
     def request_received(
-        self, obj: JSONObject, fds: OwnedFileDescriptors
+        self, obj: JSONObject, fds: FileDescriptorArray | None
     ) -> asyncio.Future[None] | None:
         try:
             try:
@@ -44,12 +44,12 @@ class VarlinkServerProtocol(VarlinkProtocol):
             return None
 
     def call_received(
-        self, call: VarlinkMethodCall, fds: OwnedFileDescriptors
+        self, call: VarlinkMethodCall, fds: FileDescriptorArray | None
     ) -> asyncio.Future[None] | None:
         """Handle a received varlink parsed as a call object and associated
-        file descriptors. The descriptors are valid until the function returns
-        None or the returned future is complete. The function should call the
-        send_reply method as needed or raise a VarlinkErrorReply to be sent by
-        the caller.
+        file descriptors. The descriptors are valid until the function returns.
+        Their life time can be extended by adding a referee before returning.
+        The function should call the send_reply method as needed or raise a
+        VarlinkErrorReply to be sent by the caller.
         """
         raise NotImplementedError

asyncvarlink/types.py
@@ -3,6 +3,7 @@
 
 """Basic type definitions."""
 
+import asyncio
 import os
 import re
 import typing
@@ -58,12 +59,68 @@ def close_fileno(thing: HasFileno) -> None:
         closemeth()
 
 
-class OwnedFileDescriptors:
-    """Represent an array of owned file descriptors."""
+class FutureCounted:
+    """A reference counting base class. References are not simply counted.
+    Instead referees are tracked individually. Any referee must be released
+    eventually by calling release. Once all referees are gone, the destroy
+    method is called once.
+    """
+
+    def __init__(self, initial: typing.Any) -> None:
+        """The constructor consumes an initial referee. Otherwise, it would be
+        immediately destroyed.
+        """
+        self._references: set[int] = {id(initial)}
+
+    def reference(self, referee: typing.Any) -> None:
+        """Record an object as referee. The referee should be either passed to
+        release once or garbage collected by Python.
+        """
+        if not self._references:
+            raise RuntimeError("cannot reference destroyed object")
+        objid = id(referee)
+        assert objid not in self._references
+        self._references.add(objid)
+
+    def reference_until_done(self, fut: asyncio.Future[typing.Any]) -> None:
+        """Reference this object until the passed future is done."""
+        self.reference(fut)
+        fut.add_done_callback(self.release)
+
+    def release(self, referee: typing.Any) -> None:
+        """Release the reference identified by the given referee. If this was
+        the last reference, this object is destroyed. Releasing a referee that
+        was not referenced is an error as is releasing a referee twice.
+        """
+        objid = id(referee)
+        try:
+            self._references.remove(objid)
+        except KeyError:
+            raise RuntimeError(
+                f"releasing reference to unregistered object {referee!r}"
+            ) from None
+        if not self._references:
+            self.destroy()
+
+    def destroy(self) -> None:
+        """Called when the last reference is released."""
+        raise NotImplementedError
+
+
+class FileDescriptorArray(FutureCounted):
+    """Represent an array of file descriptors owned and eventually released by
+    the array. The lifetime can be controlled in two ways. Responsibility for
+    individual file descriptors can be assumed by using the take method and
+    thus removing them from the array. The lifetime of the entire array can
+    be extended using the FutureCounted mechanism inherited from.
+    """
 
     def __init__(
-        self, fds: typing.Iterable[HasFileno | int | None] | None = None
+        self,
+        initial_referee: typing.Any,
+        fds: typing.Iterable[HasFileno | int | None] | None = None,
     ):
+        super().__init__(initial_referee)
         self._fds: list[HasFileno | None] = [
             fd if fd is None else FileDescriptor.upgrade(fd)
             for fd in fds or ()
@@ -74,7 +131,7 @@ class OwnedFileDescriptors:
         return any(fd is not None for fd in self._fds)
 
     def __eq__(self, other: object) -> bool:
-        if not isinstance(other, OwnedFileDescriptors):
+        if not isinstance(other, FileDescriptorArray):
             return False
         if len(self._fds) != len(other._fds):
             return False
@@ -108,6 +165,7 @@ class OwnedFileDescriptors:
                 close_fileno(fd)
 
     __del__ = close
+    destroy = close
 
 
 def validate_interface(interface: str) -> None:

tests/test_conversion.py
@@ -10,13 +10,13 @@ from asyncvarlink import (
     DictVarlinkType,
     EnumVarlinkType,
     FileDescriptor,
+    FileDescriptorArray,
     FileDescriptorVarlinkType,
     ForeignVarlinkType,
     JSONValue,
     ListVarlinkType,
     ObjectVarlinkType,
     OptionalVarlinkType,
-    OwnedFileDescriptors,
     SetVarlinkType,
     SimpleVarlinkType,
     VarlinkType,
@@ -131,11 +131,12 @@ class ConversionTests(unittest.TestCase):
         fdlist: list[int | None] = []
         oobto: dict[type, typing.Any] = {FileDescriptorVarlinkType: fdlist}
         val = vt.tojson(obj, oobto)
-        ownedfds = OwnedFileDescriptors(fdlist)
+        ownedfds = FileDescriptorArray(fdlist, fdlist)
         oobfrom: dict[type, typing.Any] = {FileDescriptorVarlinkType: ownedfds}
         obj_again = vt.fromjson(val, oobfrom)
         self.assertEqual(obj, obj_again)
         self.assertFalse(bool(ownedfds))
+        ownedfds.release(fdlist)
 
     @hypothesis.given(type_annotations, json_values)
     def test_exception(self, ta: type, val: JSONValue) -> None:

tests/test_protocol.py
@@ -6,9 +6,9 @@ import unittest
 from unittest.mock import Mock
 
 from asyncvarlink import (
+    FileDescriptorArray,
     JSONObject,
     JSONValue,
-    OwnedFileDescriptors,
     VarlinkMethodCall,
     VarlinkMethodReply,
     VarlinkProtocol,
@@ -23,9 +23,6 @@ async def wait_called(mock: Mock) -> None:
         await asyncio.sleep(0.01 * delay)
 
 
-NO_FDS = OwnedFileDescriptors()
-
-
 class TransportTests(unittest.IsolatedAsyncioTestCase):
     async def test_receive_socket(self) -> None:
         protocol = VarlinkProtocol()
@@ -37,7 +34,7 @@ class TransportTests(unittest.IsolatedAsyncioTestCase):
             )
             sock2.send(b"hello")
             await wait_called(protocol.message_received)
-        protocol.message_received.assert_called_once_with(b"hello", NO_FDS)
+        protocol.message_received.assert_called_once_with(b"hello", None)
 
     async def test_receive_socket_eof(self) -> None:
         protocol = VarlinkProtocol()
@@ -64,7 +61,7 @@ class TransportTests(unittest.IsolatedAsyncioTestCase):
         finally:
             os.close(pipe2)
             os.close(pipe1)
-        protocol.message_received.assert_called_once_with(b"hello", NO_FDS)
+        protocol.message_received.assert_called_once_with(b"hello", None)
 
     async def test_receive_pipe_eof(self) -> None:
         protocol = VarlinkProtocol()
@@ -103,10 +100,10 @@ class ProtocolTests(unittest.IsolatedAsyncioTestCase):
                 asyncio.get_running_loop(), pipe1, pipe2, protocol
             )
             protocol.request_received = Mock()
-            protocol.message_received(b'{"hello":"world"}\0', NO_FDS)
+            protocol.message_received(b'{"hello":"world"}\0', None)
             await wait_called(protocol.request_received)
             protocol.request_received.assert_called_once_with(
-                {"hello": "world"}, NO_FDS
+                {"hello": "world"}, None
             )
         finally:
             os.close(pipe2)
@@ -120,7 +117,7 @@ class ProtocolTests(unittest.IsolatedAsyncioTestCase):
                 asyncio.get_running_loop(), pipe1, pipe2, protocol
             )
             protocol.error_received = Mock(wraps=protocol.error_received)
-            protocol.message_received(b"}\0", NO_FDS)
+            protocol.message_received(b"}\0", None)
             await wait_called(protocol.error_received)
             protocol.error_received.assert_called_once()
         finally:
@@ -137,15 +134,15 @@ class ProtocolTests(unittest.IsolatedAsyncioTestCase):
             protocol.request_received = Mock(side_effect=futs)
             await asyncio.sleep(0)
             self.assertFalse(transport._paused)
-            protocol.message_received(b'{"a":0}\0{"b":0}\0', NO_FDS)
+            protocol.message_received(b'{"a":0}\0{"b":0}\0', None)
             await asyncio.sleep(0)
             self.assertTrue(transport._paused)
-            protocol.request_received.assert_called_once_with({"a": 0}, NO_FDS)
+            protocol.request_received.assert_called_once_with({"a": 0}, None)
             self.assertTrue(transport._paused)
             futs[0].set_result(None)
             await asyncio.sleep(0)
             self.assertTrue(transport._paused)
-            protocol.request_received.assert_called_with({"b": 0}, NO_FDS)
+            protocol.request_received.assert_called_with({"b": 0}, None)
             await asyncio.sleep(0)
             self.assertTrue(transport._paused)
             futs[1].set_result(None)
@@ -164,11 +161,11 @@ class ProtocolTests(unittest.IsolatedAsyncioTestCase):
             )
             protocol.request_received = Mock()
             await asyncio.sleep(0)
-            protocol.message_received(b'{"a":0}\0{"b":0}\0', NO_FDS)
+            protocol.message_received(b'{"a":0}\0{"b":0}\0', None)
             await wait_called(protocol.request_received)
-            protocol.request_received.assert_called_once_with({"a": 0}, NO_FDS)
+            protocol.request_received.assert_called_once_with({"a": 0}, None)
             await asyncio.sleep(0)
-            protocol.request_received.assert_called_with({"b": 0}, NO_FDS)
+            protocol.request_received.assert_called_with({"b": 0}, None)
         finally:
             os.close(pipe2)
             os.close(pipe1)

commit b80ae86c7012f8c89c459f961fac85959b880d6d

FileDescriptorVarlinkType: discard useless branch
The error message does not match the test condition and when fds is
None, the next condition will raise a better ConversionError.

Fixes: 439d69049a02 ("refactor the lifetime management of file descriptor arrays")

asyncvarlink/conversion.py
@@ -678,10 +678,6 @@ class FileDescriptorVarlinkType(VarlinkType):
                 "cannot unrepresent a file descriptor without oobstate"
             )
         fds = self._get_oob(oobstate)
-        if fds is None:
-            raise ConversionError(
-                f"attempt to convert invalid file descriptor index {obj}"
-            )
         if not isinstance(fds, FileDescriptorArray):
             raise ConversionError(
                 f"out-of-band state for {self.__class__.__name__} should be "
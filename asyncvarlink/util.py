helmutg/asyncvarlink

commit 9a8a7d4109fbd1189c7cc5d5c7d8fa91a9636eb3

add create_unix_server function
We already have connect_unix_varlink and this is the common counterpart.

asyncvarlink/util.py
@@ -7,9 +7,10 @@ import asyncio
 import contextlib
 import os
 import socket
+import stat
 import typing
 
-from .protocol import VarlinkProtocol, VarlinkTransport
+from .protocol import _BLOCKING_ERRNOS, VarlinkProtocol, VarlinkTransport
 from .types import FileDescriptor
 
 
@@ -69,6 +70,110 @@ async def connect_unix_varlink(
     transport = VarlinkTransport(loop, sock, sock, protocol)
     await asyncio.sleep(0)  # wait for all call_soon
     return transport, protocol
+
+
+class VarlinkUnixServer(asyncio.AbstractServer):
+    """An asyncio server class that will construct VarlinkTransports for
+    accepted connections.
+    """
+
+    def __init__(
+        self,
+        loop: asyncio.AbstractEventLoop,
+        sock: socket.socket,
+        protocol_factory: typing.Callable[[], VarlinkProtocol],
+    ):
+        self._loop = loop
+        self._sock = sock
+        self._protocol_factory = protocol_factory
+        self._serving: asyncio.Future[None] | None = None
+
+    def close(self) -> None:
+        if self._serving is not None:
+            self._loop.remove_reader(self._sock)
+            self._serving.set_result(None)
+            self._serving = None
+
+    def get_loop(self) -> asyncio.AbstractEventLoop:
+        return self._loop
+
+    def is_serving(self) -> bool:
+        return self._serving is not None
+
+    async def start_serving(self) -> None:
+        if self._serving is None:
+            self._serving = self._loop.create_future()
+            self._sock.listen(1)
+            self._loop.add_reader(self._sock, self._handle_accept)
+
+    def _handle_accept(self) -> None:
+        try:
+            conn, addr = self._sock.accept()
+        except OSError as err:
+            if err.errno in _BLOCKING_ERRNOS:
+                return
+            self.close()
+            raise
+        conn.setblocking(False)
+        VarlinkTransport(
+            self._loop,
+            conn,
+            conn,
+            self._protocol_factory(),
+            {"peername": addr},
+        )
+
+    async def serve_forever(self) -> None:
+        await self.start_serving()
+        await self.wait_closed()
+
+    async def wait_closed(self) -> None:
+        if self._serving is None:
+            return
+        await self._serving
+
+
+async def create_unix_server(
+    loop: asyncio.AbstractEventLoop,
+    protocol_factory: typing.Callable[[], VarlinkProtocol],
+    path: os.PathLike[str] | None = None,
+    *,
+    sock: socket.socket | None = None,
+    start_serving: bool = True,
+) -> VarlinkUnixServer:
+    """In a similar spirit to asyncio.SelectorEventLoop.create_unix_server
+    create a UNIX domain socket server except that the transport class being
+    used will be VarlinkTransport for being capable of sending and receiving
+    file descriptors.
+
+    Either the socket (sock) or path must be provided.
+    """
+    if sock is None:
+        if path is None:
+            raise ValueError("neither path nor sock specified")
+        pathstr = os.fspath(path)
+        sock = socket.socket(
+            socket.AF_UNIX, socket.SOCK_STREAM | socket.SOCK_NONBLOCK
+        )
+        if not pathstr.startswith("\0"):
+            try:
+                st = os.stat(pathstr)
+            except FileNotFoundError:
+                pass
+            else:
+                if stat.S_ISSOCK(st.st_mode):
+                    os.unlink(pathstr)
+        try:
+            sock.bind(pathstr)
+        except:
+            sock.close()
+            raise
+    else:
+        sock.setblocking(False)
+    server = VarlinkUnixServer(loop, sock, protocol_factory)
+    if start_serving:
+        await server.start_serving()
+    return server
 
 
 def get_listen_fd(name: str) -> typing.Optional[FileDescriptor]:

commit eec373dd8e80ee3c7396d2c1988348fdda3c27e5

VarlinkUnixServer.close should actually close
Fixes: 9a8a7d4109fb ("add create_unix_server function")

asyncvarlink/util.py
@@ -100,7 +100,7 @@ class VarlinkUnixServer(asyncio.AbstractServer):
         loop: asyncio.AbstractEventLoop | None = None,
     ):
         self._loop = asyncio.get_running_loop() if loop is None else loop
-        self._sock = sock
+        self._sock: socket.socket | None = sock
         self._protocol_factory = protocol_factory
         self._serving: asyncio.Future[None] | None = None
         self._transports: weakref.WeakSet[VarlinkTransport] = weakref.WeakSet()
@@ -108,7 +108,10 @@ class VarlinkUnixServer(asyncio.AbstractServer):
     @override
     def close(self) -> None:
         if self._serving is not None:
+            assert self._sock is not None
             self._loop.remove_reader(self._sock)
+            self._sock.close()
+            self._sock = None
             self._serving.set_result(None)
             self._serving = None
 
@@ -123,11 +126,15 @@ class VarlinkUnixServer(asyncio.AbstractServer):
     @override
     async def start_serving(self) -> None:
         if self._serving is None:
+            if self._sock is None:
+                raise ValueError("cannot start serving on closed server")
             self._serving = self._loop.create_future()
             self._sock.listen(1)
             self._loop.add_reader(self._sock, self._handle_accept)
 
     def _handle_accept(self) -> None:
+        if self._sock is None:
+            return  # closed before handler could run
         try:
             conn, addr = self._sock.accept()
         except OSError as err:
helmutg/asyncvarlink

commit 58379248a0b62805b3e23c7b5a75091b797f6434

VarlinkInterfaceProxy: relax __call__ method signature
The precise form is annoying to use. Whenever one performs a proxied
call, mypy is unhappy about await as it could be a generator and unhappy
about async for as it could be an awaitable. Relaxing this silences
those warnings as before the _ProxiedVarlinkMethodBase was introduced.

asyncvarlink/clientprotocol.py
@@ -249,10 +249,9 @@ class _ProxiedVarlinkMethodBase:
 
     def __call__(
         self, **kwargs: typing.Any
-    ) -> (
-        collections.abc.Awaitable[typing.Any]
-        | collections.abc.AsyncIterator[typing.Any]
-    ):
+    ) -> typing.Any:
+        # This really returns either an Awaitable or an AsyncGenerator, but
+        # typing it like that makes using it very annoying.
         raise NotImplementedError
 
 

commit 9982880621d71092ee542360311972d019218e07

fold call to please black
Fixes: 58379248a0b6 ("VarlinkInterfaceProxy: relax __call__ method signature")

asyncvarlink/clientprotocol.py
@@ -247,9 +247,7 @@ class _ProxiedVarlinkMethodBase:
             self._protocol, self._interface, self._fqmethod, self._signature
         )
 
-    def __call__(
-        self, **kwargs: typing.Any
-    ) -> typing.Any:
+    def __call__(self, **kwargs: typing.Any) -> typing.Any:
         # This really returns either an Awaitable or an AsyncGenerator, but
         # typing it like that makes using it very annoying.
         raise NotImplementedError
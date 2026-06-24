helmutg/asyncvarlink

commit 08f395586e4945beb0abd807f7da9c2675a6e3c8

varlinkmethod: improve handling of optional parameters
Parameters can be optional in multiple ways on the Python side. An
obvious ways is having its type union with None and assigning a None
default. We also treat any parameter with a default value as optional
though. When a client passes null there, it is converted to None and
passed via the defaulted parameter, which may not be accepting None
values. In a similar way, a client may skip sending an optional value
and we may fail passing the mandatory on the Python side.

Both of these constitute sensible situations. Therefore extend the
ObjectVarlinkType to cover these parameter-specifics. The additional
capabilities are only exercised when constructing mappings used as
method parameters and provide tighter control about the emission of None
values.

asyncvarlink/conversion.py
@@ -444,8 +444,35 @@ class ObjectVarlinkType(VarlinkType):
     def __init__(
         self,
         typemap: dict[str, VarlinkType],
+        *,
+        populatenone: collections.abc.Collection[str] = frozenset(),
+        discardnone: collections.abc.Collection[str] = frozenset(),
     ):
+        """Construct an representation of a varlink object from a given mapping
+        from keys to type representations. Optionally, two further sets may be
+        given. Fields that are to be found in the populatenone set shall always
+        be emitted as None by fromjson even if missing in the source. In a
+        similar vein, fields that are found in the discardnone set, are removed
+        by fromjson if their value is None.
+        """
+        non_optional = {
+            name
+            for name, tobj in typemap.items()
+            if not isinstance(tobj, OptionalVarlinkType)
+        }
+        if excess := non_optional.intersection(populatenone):
+            raise ValueError(
+                f"non-optional fields {excess} must not be listed in "
+                "populatenone"
+            )
+        if excess := non_optional.intersection(discardnone):
+            raise ValueError(
+                f"non-optional fields {excess} must not be listed in "
+                "discardnone"
+            )
         self._typemap = typemap
+        self._populatenone = populatenone
+        self._discardnone = discardnone
         # mypy cannot runtime-constructed type hints.
         self.as_type = typing.TypedDict(  # type: ignore[misc]
             "ObjectVarlinkTypedDict",
@@ -494,12 +521,16 @@ class ObjectVarlinkType(VarlinkType):
                 value = obj[key]
             except KeyError as err:
                 if isinstance(vtype, OptionalVarlinkType):
+                    if key in self._populatenone:
+                        result[key] = None
                     continue
                 raise ConversionError(
                     f"missing required key {key} in given dict"
                 ) from err
             with ConversionError.context(key):
-                result[key] = vtype.fromjson(value, oobstate)
+                pyvalue = vtype.fromjson(value, oobstate)
+            if pyvalue is not None or key not in self._discardnone:
+                result[key] = pyvalue
         for key in obj:
             if key not in self._typemap:
                 raise ConversionError(f"no type for key {key}", [key])

asyncvarlink/interface.py
@@ -12,6 +12,7 @@ import typing
 from .conversion import (
     ForeignVarlinkType,
     ObjectVarlinkType,
+    OptionalVarlinkType,
     VarlinkType,
     _merge_typedefs,
 )
@@ -86,19 +87,33 @@ _MethodResultType = (
 )
 
 
-def _param_to_varlinkobj(
-    name: str, tobj: inspect.Parameter, *, allow_foreign: bool = False
-) -> VarlinkType:
-    vtype = VarlinkType.from_type_annotation(tobj.annotation)
-    if (not allow_foreign) and any(
-        isinstance(vt, ForeignVarlinkType) for vt in vtype.traverse()
-    ):
-        raise TypeError(
-            f"Use of foreign not enabled for parameter {name} typed {tobj.annotation!r}"
-        )
-    if tobj.default is tobj.empty:
-        return vtype
-    return vtype.optional()
+def _params_to_varlinkobj(
+    params: collections.abc.Iterator[tuple[str, inspect.Parameter]],
+    *,
+    allow_foreign: bool = False,
+) -> ObjectVarlinkType:
+    typemap = {}
+    populatenone = set()
+    discardnone = set()
+    for name, tobj in params:
+        vtype = VarlinkType.from_type_annotation(tobj.annotation)
+        if (not allow_foreign) and any(
+            isinstance(vt, ForeignVarlinkType) for vt in vtype.traverse()
+        ):
+            raise TypeError(
+                f"Use of foreign not enabled for parameter {name} typed "
+                f"{tobj.annotation!r}"
+            )
+        if tobj.default is tobj.empty:
+            typemap[name] = vtype
+            if isinstance(vtype, OptionalVarlinkType):
+                populatenone.add(name)
+        else:
+            typemap[name] = vtype.optional()
+            discardnone.add(name)
+    return ObjectVarlinkType(
+        typemap, populatenone=populatenone, discardnone=discardnone
+    )
 
 
 class _VarlinkMethodDecorator(typing.Protocol):
@@ -291,14 +306,7 @@ def varlinkmethod(
         vlsig = VarlinkMethodSignature(
             asyncgen or asynchronous,
             more,
-            ObjectVarlinkType(
-                {
-                    name: _param_to_varlinkobj(
-                        name, tobj, allow_foreign=allow_foreign
-                    )
-                    for name, tobj in param_iterator
-                },
-            ),
+            _params_to_varlinkobj(param_iterator, allow_foreign=allow_foreign),
             return_vtype,
         )
 

tests/test_serverprotocol.py
@@ -93,6 +93,14 @@ class DemoInterface(VarlinkInterface, name="com.example.demo"):
             data = await asyncio.get_running_loop().sock_recv(sock, 1024)
             return data.decode("utf8")
 
+    @varlinkmethod(return_parameter="error")
+    def Optional(self, *, optnone: int | None, optdef: int = 42) -> str | None:
+        if optnone is not None:
+            return "optnone is not None"
+        if optdef != 42:
+            return "optdef != 42"
+        return None
+
 
 class ServerTests(unittest.IsolatedAsyncioTestCase):
     @override
@@ -335,3 +343,9 @@ class ServerTests(unittest.IsolatedAsyncioTestCase):
             self.fut.set_result(42)
             await defer(until_called=self.protocol.connection_lost)
             self.protocol.connection_lost.assert_called_once_with(None)
+
+    async def test_optional(self) -> None:
+        await self.invoke(
+            b'{"method":"com.example.demo.Optional","parameters":{"optdef":null}}',
+            b'{"parameters":{"error":null}}',
+        )

commit f90f81865b5a28f8e45d723c1dfb3d70c4ea252e

conversion: fix imprecise ConversionError in ObjectVarlinkType.fromjson
Fixes: 08f395586e49 ("varlinkmethod: improve handling of optional parameters")

asyncvarlink/conversion.py
@@ -534,7 +534,7 @@ class ObjectVarlinkType(VarlinkType):
                         result[key] = None
                     continue
                 raise ConversionError(
-                    f"missing required key {key} in given dict"
+                    f"missing required key {key} in given dict", [key]
                 ) from err
             with ConversionError.context(key):
                 pyvalue = vtype.fromjson(value, oobstate)
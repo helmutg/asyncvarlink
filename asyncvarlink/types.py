helmutg/asyncvarlink

commit 36c0eea73880e95860f61b740530597010e64d76

add classes VarlinkMethod{Call,Reply} for parsing relevant JSON objects

asyncvarlink/types.py
@@ -3,6 +3,7 @@
 
 """Basic type definitions."""
 
+import re
 import typing
 
 
@@ -32,3 +33,20 @@ class FileDescriptor(int):
     def fileno(self) -> int:
         """Returns the underlying file descriptor, i.e. self."""
         return self
+
+
+def validate_interface(interface: str) -> None:
+    """Validate a varlink interface in reverse-domain notation. May raise a
+    ValueError.
+    """
+    if not re.match(
+        r"[A-Za-z](?:-*[A-Za-z0-9])*(?:\.[A-Za-z0-9](?:-*[A-Za-z0-9])*)+",
+        interface,
+    ):
+        raise ValueError(f"invalid varlink interface {interface!r}")
+
+
+def validate_name(name: str) -> None:
+    """Validate a varlink name. May raise a ValueError."""
+    if not re.match(r"^[A-Z][A-Za-z0-9]*$", name):
+        raise ValueError(f"invalid varlink name {name!r}")

asyncvarlink/protocol.py
@@ -5,6 +5,7 @@
 
 import asyncio
 import collections
+import dataclasses
 import errno
 import functools
 import json
@@ -13,7 +14,14 @@ import os
 import socket
 import typing
 
-from .types import FileDescriptor, HasFileno, JSONObject
+from .types import (
+    FileDescriptor,
+    HasFileno,
+    JSONObject,
+    JSONValue,
+    validate_interface,
+    validate_name,
+)
 
 logger = logging.getLogger("asyncvarlink.protocol")
 
@@ -419,3 +427,180 @@ class VarlinkProtocol(asyncio.BaseProtocol):
 
     def connection_lost(self, exc: Exception | None) -> None:
         pass
+
+
+@dataclasses.dataclass
+class VarlinkMethodCall:
+    """Represent a parsed and roughly validated varlink method call."""
+
+    method: str
+    parameters: JSONObject
+    oneway: bool = False
+    more: bool = False
+    upgrade: bool = False
+    extensions: JSONObject = dataclasses.field(default_factory=dict)
+
+    @property
+    def method_interface(self) -> str:
+        """Return the interface portion of the method string. May raise a
+        ValueError.
+        """
+        interface, dot, _ = self.method.rpartition(".")
+        if dot != ".":
+            raise ValueError("unqualified method string")
+        return interface
+
+    @property
+    def method_name(self) -> str:
+        """Return the unqualified name portion of the method string."""
+        return self.method.rpartition(".")[2]
+
+    def __post_init__(self) -> None:
+        validate_interface(self.method_interface)
+        validate_name(self.method_name)
+
+    @classmethod
+    def fromjson(cls, obj: JSONValue) -> "VarlinkMethodCall":
+        """Parse a JSON value into a validated VarlinkMethodCall. May raise
+        TypeError and ValueError.
+        """
+        if not isinstance(obj, dict):
+            raise TypeError(
+                f"call object must be a map, is {obj.__class__.__name__}"
+            )
+        extensions = obj.copy()
+        try:
+            method = extensions.pop("method")
+        except KeyError:
+            raise ValueError("call object must have a method") from None
+        if not isinstance(method, str):
+            raise TypeError(
+                f"method field of call object must be a str, is "
+                f"{method.__class__.__name__}"
+            )
+        parameters = extensions.pop("parameters", {})
+        if not isinstance(parameters, dict):
+            raise TypeError(
+                f"call parameters must be map, are "
+                f"{parameters.__class__.__name__}"
+            )
+        oneway = extensions.pop("oneway", False)
+        if not isinstance(oneway, bool):
+            raise TypeError(
+                f"call property oneay must be bool, is "
+                f"{oneway.__class__.__name__}"
+            )
+        more = extensions.pop("more", False)
+        if not isinstance(more, bool):
+            raise TypeError(
+                f"call property more must be bool, is "
+                f"{oneway.__class__.__name__}"
+            )
+        upgrade = extensions.pop("upgrade", False)
+        if not isinstance(upgrade, bool):
+            raise TypeError(
+                f"call property upgrade must be bool, is "
+                f"{oneway.__class__.__name__}"
+            )
+        if sum((oneway, more, upgrade)) > 1:
+            raise ValueError("cannot combine oneway, more or upgrade")
+        return cls(
+            method,
+            parameters,
+            oneway,
+            more,
+            upgrade,
+            extensions,
+        )
+
+    def tojson(self) -> JSONObject:
+        """Export as a JSONObject suitable for json.dumps."""
+        result: JSONObject = {"method": self.method}
+        if self.parameters:
+            result["parameters"] = self.parameters
+        if self.oneway:
+            result["oneway"] = True
+        if self.more:
+            result["more"] = True
+        if self.upgrade:
+            result["upgrade"] = True
+        result.update(self.extensions)
+        return result
+
+
+@dataclasses.dataclass
+class VarlinkMethodReply:
+    """Represent a parsed and roughly validated varlink method reply."""
+
+    parameters: JSONObject
+    continues: bool = False
+    error: str | None = None
+    extensions: JSONObject = dataclasses.field(default_factory=dict)
+
+    @property
+    def error_interface(self) -> str:
+        """Return the interface portion of the error string if any. May raise a
+        ValueError.
+        """
+        if self.error is None:
+            raise ValueError("not an error")
+        interface, dot, _ = self.error.rpartition(".")
+        if dot != ".":
+            raise ValueError("unqualified error string")
+        return interface
+
+    @property
+    def error_name(self) -> str:
+        """Return the unqualified name portion of the error string if any. May
+        raise a ValueError.
+        """
+        if self.error is None:
+            raise ValueError("not an error")
+        return self.error.rpartition(".")[2]
+
+    def __post_init__(self) -> None:
+        if self.error is not None:
+            validate_interface(self.error_interface)
+            validate_name(self.error_name)
+
+    @classmethod
+    def fromjson(cls, obj: JSONValue) -> "VarlinkMethodReply":
+        """Parse a JSON value into a validated VarlinkMethodReply. May raise
+        TypeError and ValueError.
+        """
+        if not isinstance(obj, dict):
+            raise TypeError(
+                f"call object must be a map, is {obj.__class__.__name__}"
+            )
+        extensions = obj.copy()
+        parameters = extensions.pop("parameters", {})
+        if not isinstance(parameters, dict):
+            raise TypeError(
+                f"reply parameters must be map, are "
+                f"{parameters.__class__.__name__}"
+            )
+        continues = extensions.pop("continues", False)
+        if not isinstance(continues, bool):
+            raise TypeError(
+                f"reply property continues must be bool, is "
+                f"{continues.__class__.__name__}"
+            )
+        error = extensions.pop("error", None)
+        if error is not None and not isinstance(error, str):
+            raise TypeError(
+                f"reply property error must be str, is "
+                f"{error.__class__.__name__}"
+            )
+        return VarlinkMethodReply(parameters, continues, error, extensions)
+
+    def tojson(self) -> JSONObject:
+        """Export as a JSONObject suitable for json.dumps."""
+        result: JSONObject = {}
+        if self.continues:
+            result["continues"] = True
+        if self.error:
+            result["error"] = self.error
+        if self.parameters:
+            result["parameters"] = self.parameters
+        result.update(self.extensions)
+        return result

tests/test_protocol.py
@@ -5,7 +5,12 @@ import socket
 import unittest
 from unittest.mock import Mock
 
-from asyncvarlink import VarlinkProtocol, VarlinkTransport
+from asyncvarlink import (
+    VarlinkMethodCall,
+    VarlinkMethodReply,
+    VarlinkProtocol,
+    VarlinkTransport,
+)
 
 
 async def wait_called(mock: Mock) -> None:
@@ -175,3 +180,60 @@ class ProtocolTests(unittest.IsolatedAsyncioTestCase):
             self.assertEqual(
                 b'{"hello": "world"}\0', await loop.sock_recv(sock2, 1024)
             )
+
+
+class CallReplyTest(unittest.TestCase):
+    def test_valid_call(self) -> None:
+        for jobj in (
+            {},
+            {"parameters": {"ingredient": "egg"}},
+            {"oneway": True},
+            {"more": True},
+            {"upgrade": True},
+            {"more": True},
+            {"com.example.Extension": "spam"},
+        ):
+            jobj["method"] = "com.example.Spam"
+            call = VarlinkMethodCall.fromjson(jobj)
+            self.assertEqual(jobj, call.tojson())
+
+    def test_invalid_call(self) -> None:
+        for exc, jobj in (
+            (TypeError, 42),
+            (ValueError, {}),
+            (TypeError, {"method": 42}),
+            (ValueError, {"method": "Unqualified"}),
+            (ValueError, {"method": "com.example.lowercase"}),
+            (TypeError, {"method": "com.example.Spam", "oneway": "egg"}),
+            (TypeError, {"method": "com.example.Spam", "more": "egg"}),
+            (TypeError, {"method": "com.example.Spam", "upgrade": "egg"}),
+            (
+                ValueError,
+                {"method": "com.example.Spam", "more": True, "oneway": True},
+            ),
+            (TypeError, {"method": "com.example.Spam", "parameters": "egg"}),
+        ):
+            with self.assertRaises(exc):
+                VarlinkMethodCall.fromjson(jobj)
+
+    def test_valid_reply(self) -> None:
+        for jobj in (
+            {},
+            {"error": "com.example.Spam"},
+            {"parameters": {"ingredients": "egg"}},
+            {"continues": True},
+        ):
+            reply = VarlinkMethodReply.fromjson(jobj)
+            self.assertEqual(jobj, reply.tojson())
+
+    def test_invalid_reply(self) -> None:
+        for exc, jobj in (
+            (TypeError, 42),
+            (TypeError, {"error": 42}),
+            (ValueError, {"error": "Unqualified"}),
+            (ValueError, {"error": "com.example.lowercase"}),
+            (TypeError, {"parameters": "egg"}),
+            (TypeError, {"continues": "egg"}),
+        ):
+            with self.assertRaises(exc):
+                VarlinkMethodReply.fromjson(jobj)

commit 2730c099e80328ad22fea341fa013b642d121846

validate_interface: fix acceptance of garbage at end
Fixes: 36c0eea73880 ("add classes VarlinkMethod{Call,Reply} for parsing relevant JSON objects")

asyncvarlink/types.py
@@ -422,7 +422,7 @@ def validate_interface(interface: str) -> None:
     ValueError.
     """
     if not re.match(
-        r"[A-Za-z](?:-*[A-Za-z0-9])*(?:\.[A-Za-z0-9](?:-*[A-Za-z0-9])*)+",
+        r"^[A-Za-z](?:-*[A-Za-z0-9])*(?:\.[A-Za-z0-9](?:-*[A-Za-z0-9])*)+$",
         interface,
     ):
         raise ValueError(f"invalid varlink interface {interface!r}")